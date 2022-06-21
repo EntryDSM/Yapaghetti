5e858359aec08c3c8406a894526fe6194f6e52f6
정대현 <eogus_2513@dsm.hs.kr>
2022-06-22 02:02:29 +0900

    :gear: :: Initial commit

---
+++ src/main/java/kr/hs/entrydsm/yapaghetti/YapaghettiApplication.java
@@ -0,0 +1,13 @@
+package kr.hs.entrydsm.yapaghetti;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YapaghettiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(YapaghettiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/kr/hs/entrydsm/yapaghetti/YapaghettiApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.hs.entrydsm.yapaghetti;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YapaghettiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
