b1b9bd8f75d53fb68042db620a22a2a1dd6da74e
정대현 <eogus_2513@dsm.hs.kr>
2022-06-29 22:18:33 +0900

    ⚡️ :: (#3) implenents ErrorProperty & 필드 추가

--- yapaghetti-application/src/main/java/kr/hs/entrydsm/yapaghetti/user/error/UserErrorCode.java
+++ yapaghetti-application/src/main/java/kr/hs/entrydsm/yapaghetti/user/error/UserErrorCode.java
@@ -1,4 +1,14 @@
 package kr.hs.entrydsm.yapaghetti.user.error;
 
-public enum UserErrorCode {
+import kr.hs.entrydsm.yapaghetti.error.ErrorProperty;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public enum UserErrorCode implements ErrorProperty {
+    ;
+
+    private final int status;
+    private final String message;
 }
