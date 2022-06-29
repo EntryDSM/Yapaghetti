fb0b7e68e0e6682a491f68d3f9a6c5178852ed55
정대현 <eogus_2513@dsm.hs.kr>
2022-06-30 07:48:13 +0900

    :recycle: :: (#3) 추상 클래스로 변경

--- yapaghetti-application/src/main/java/kr/hs/entrydsm/yapaghetti/error/YapaghettiException.java
+++ yapaghetti-application/src/main/java/kr/hs/entrydsm/yapaghetti/error/YapaghettiException.java
@@ -5,7 +5,7 @@ import lombok.RequiredArgsConstructor;
 
 @Getter
 @RequiredArgsConstructor
-public class YapaghettiException extends RuntimeException {
+public abstract class YapaghettiException extends RuntimeException {
 
     private final ErrorProperty errorProperty;
 }
