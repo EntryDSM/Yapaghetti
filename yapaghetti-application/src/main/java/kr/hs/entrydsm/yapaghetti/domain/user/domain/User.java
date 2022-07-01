9d8d65e9410ad476cd3818916d263d179241842d
leeseojune53 <sung07288346@gmail.com>
2022-07-01 23:20:01 +0900

    ⚡️ :: User Aggregate field

--- yapaghetti-application/src/main/java/kr/hs/entrydsm/yapaghetti/domain/user/domain/User.java
+++ yapaghetti-application/src/main/java/kr/hs/entrydsm/yapaghetti/domain/user/domain/User.java
@@ -4,8 +4,31 @@ import kr.hs.entrydsm.yapaghetti.annotation.Aggregate;
 import lombok.Builder;
 import lombok.Getter;
 
+import java.util.UUID;
+
 @Aggregate
 @Getter
 @Builder
 public class User {
+
+    private final Long id;
+
+    private final String email;
+
+    private final String password;
+
+    private final String name;
+
+    private final String profileImagePath;
+
+    private final String phoneNumber;
+
+    private final String location;
+
+    private final boolean isVisited;
+
+    private final UUID publicId;
+
+    private final UserRole role;
+
 }
