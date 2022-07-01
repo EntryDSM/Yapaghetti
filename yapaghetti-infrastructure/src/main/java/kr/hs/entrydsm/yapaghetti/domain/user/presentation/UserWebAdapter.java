c290ce524f9acea7589576c50a3b2b455e0af13a
leeseojune53 <sung07288346@gmail.com>
2022-07-02 00:14:06 +0900

    ⚡️ :: POST /users/auth

--- yapaghetti-infrastructure/src/main/java/kr/hs/entrydsm/yapaghetti/domain/user/presentation/UserWebAdapter.java
+++ yapaghetti-infrastructure/src/main/java/kr/hs/entrydsm/yapaghetti/domain/user/presentation/UserWebAdapter.java
@@ -1,4 +1,32 @@
 package kr.hs.entrydsm.yapaghetti.domain.user.presentation;
 
+import kr.hs.entrydsm.yapaghetti.domain.user.api.SignInPort;
+import kr.hs.entrydsm.yapaghetti.domain.user.api.dto.request.DomainSignInRequest;
+import kr.hs.entrydsm.yapaghetti.domain.user.api.dto.response.SignInResponse;
+import kr.hs.entrydsm.yapaghetti.domain.user.presentation.dto.request.WebSignInRequest;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.validation.Valid;
+
+@RequiredArgsConstructor
+@RequestMapping("/users")
+@RestController
 public class UserWebAdapter {
+
+    private final SignInPort signInPort;
+
+    @PostMapping("/auth")
+    public SignInResponse signIn(@Valid WebSignInRequest request) {
+        return signInPort.signIn(
+                new DomainSignInRequest(
+                        request.getEmail(),
+                        request.getPassword(),
+                        request.getUserType()
+                )
+        );
+    }
+
 }
