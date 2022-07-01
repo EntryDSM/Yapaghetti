eac81ff002994b71a761cfd9f0a9f18d397abc9d
leeseojune53 <sung07288346@gmail.com>
2022-07-01 23:19:21 +0900

    ⚡️ :: UserErrorCode enum

--- yapaghetti-application/src/main/java/kr/hs/entrydsm/yapaghetti/domain/user/error/UserErrorCode.java
+++ yapaghetti-application/src/main/java/kr/hs/entrydsm/yapaghetti/domain/user/error/UserErrorCode.java
@@ -8,7 +8,8 @@ import lombok.Getter;
 @Getter
 @AllArgsConstructor(access = AccessLevel.PRIVATE)
 public enum UserErrorCode implements ErrorProperty {
-    ;
+    USER_INVALID_PASSWORD(401, "잘못된 비밀번호입니다."),
+    USER_NOT_FOUND(404, "사용자를 찾을 수 없습니다.");
 
     private final int status;
     private final String message;
