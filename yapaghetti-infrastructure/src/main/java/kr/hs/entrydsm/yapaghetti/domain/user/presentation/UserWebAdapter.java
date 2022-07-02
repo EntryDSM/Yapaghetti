c9574de5ce84d122e2c76d4f0426774c99beaea3
leeseojune53 <sung07288346@gmail.com>
2022-07-02 23:11:24 +0900

    ⚡️ :: PATCH /users/first-password

--- yapaghetti-infrastructure/src/main/java/kr/hs/entrydsm/yapaghetti/domain/user/presentation/UserWebAdapter.java
+++ yapaghetti-infrastructure/src/main/java/kr/hs/entrydsm/yapaghetti/domain/user/presentation/UserWebAdapter.java
@@ -1,13 +1,19 @@
 package kr.hs.entrydsm.yapaghetti.domain.user.presentation;
 
+import kr.hs.entrydsm.yapaghetti.domain.user.api.FirstPasswordPort;
 import kr.hs.entrydsm.yapaghetti.domain.user.api.SignInPort;
+import kr.hs.entrydsm.yapaghetti.domain.user.api.dto.request.DomainFirstPasswordRequest;
 import kr.hs.entrydsm.yapaghetti.domain.user.api.dto.request.DomainSignInRequest;
 import kr.hs.entrydsm.yapaghetti.domain.user.api.dto.response.SignInResponse;
+import kr.hs.entrydsm.yapaghetti.domain.user.presentation.dto.request.WebFirstPasswordRequest;
 import kr.hs.entrydsm.yapaghetti.domain.user.presentation.dto.request.WebSignInRequest;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.validation.Valid;
@@ -18,6 +24,7 @@ import javax.validation.Valid;
 public class UserWebAdapter {
 
     private final SignInPort signInPort;
+    private final FirstPasswordPort firstPasswordPort;
 
     @PostMapping("/auth")
     public SignInResponse signIn(@RequestBody @Valid WebSignInRequest request) {
@@ -28,6 +35,17 @@ public class UserWebAdapter {
                         .userType(request.getUserType())
                         .build()
         );
+    }
+
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    @PatchMapping("/first-password")
+    public void firstPassword(@RequestBody @Valid WebFirstPasswordRequest request) {
+        firstPasswordPort.firstPassword(
+                DomainFirstPasswordRequest.builder()
+                        .password(request.getPassword())
+                        .newPassword(request.getNewPassword())
+                        .build()
+                );
     }
 
 }
