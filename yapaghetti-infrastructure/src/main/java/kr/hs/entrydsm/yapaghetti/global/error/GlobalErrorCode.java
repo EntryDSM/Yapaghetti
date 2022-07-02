3ab3d68d6f989e320ea89faca3fa424d3af64058
leeseojune53 <sung07288346@gmail.com>
2022-07-02 20:59:40 +0900

    ⚡️ :: GlobalErrorCode

--- yapaghetti-infrastructure/src/main/java/kr/hs/entrydsm/yapaghetti/global/error/GlobalErrorCode.java
+++ yapaghetti-infrastructure/src/main/java/kr/hs/entrydsm/yapaghetti/global/error/GlobalErrorCode.java
@@ -10,6 +10,8 @@ import lombok.Getter;
 public enum GlobalErrorCode implements ErrorProperty {
 
     IMAGE_EXTENSION_INVALID(400, "사진의 확장자는 jpg, jpeg, png, heic이어야 합니다."),
+    INVALID_TOKEN(401, "유효하지 않은 토큰입니다."),
+    INVALID_TOKEN_TYPE(401, "유효하지 않은 토큰 타입입니다."),
     IMAGE_NOT_FOUND(404, "사진을 찾을 수 없습니다."),
     INTERNAL_SERVER_ERROR(500, "서버 오류");
 
