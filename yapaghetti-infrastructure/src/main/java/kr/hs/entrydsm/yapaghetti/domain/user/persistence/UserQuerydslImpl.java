f8166e0a0e8c7810aadbc9bacd755e9594bfd564
이승윤 <yyuunn17@naver.com>
2022-07-02 22:20:50 +0900

    ♻️ :: (#33) QuerydslRepositorySupport 말고 jpaQueryFactory 사용

--- yapaghetti-infrastructure/src/main/java/kr/hs/entrydsm/yapaghetti/domain/user/persistence/UserQuerydslImpl.java
+++ yapaghetti-infrastructure/src/main/java/kr/hs/entrydsm/yapaghetti/domain/user/persistence/UserQuerydslImpl.java
@@ -1,14 +1,13 @@
 package kr.hs.entrydsm.yapaghetti.domain.user.persistence;
 
-import kr.hs.entrydsm.yapaghetti.domain.user.domain.User;
-import org.springframework.data.jpa.repository.support.QuerydslRepositorySupport;
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
 
+@RequiredArgsConstructor
 @Repository
-public class UserQuerydslImpl extends QuerydslRepositorySupport implements UserQuerydslExtension {
+public class UserQuerydslImpl implements UserQuerydslExtension {
 
-    public UserQuerydslImpl() {
-        super(User.class);
-    }
+    private final JPAQueryFactory jpaQueryFactory;
 
 }
