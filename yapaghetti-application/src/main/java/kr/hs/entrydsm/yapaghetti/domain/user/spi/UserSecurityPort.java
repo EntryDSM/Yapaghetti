cbfd79f585d6215a1a326a1e12f9dc8b81d33581
leeseojune53 <sung07288346@gmail.com>
2022-07-02 23:06:16 +0900

    ⚡️ :: encodePassword, getCurrentUserPublicId

--- yapaghetti-application/src/main/java/kr/hs/entrydsm/yapaghetti/domain/user/spi/UserSecurityPort.java
+++ yapaghetti-application/src/main/java/kr/hs/entrydsm/yapaghetti/domain/user/spi/UserSecurityPort.java
@@ -1,5 +1,9 @@
 package kr.hs.entrydsm.yapaghetti.domain.user.spi;
 
+import java.util.UUID;
+
 public interface UserSecurityPort {
+    String encodePassword(String password);
     boolean matches(String rawPassword, String encodedPassword);
+    UUID getCurrentUserPublicId();
 }
