da90bccf28c504348842eef6c64106a27a5e5aa5
leeseojune53 <sung07288346@gmail.com>
2022-07-02 21:10:24 +0900

    ⚡️ :: filterConfig apply

--- yapaghetti-infrastructure/src/main/java/kr/hs/entrydsm/yapaghetti/global/config/SecurityConfig.java
+++ yapaghetti-infrastructure/src/main/java/kr/hs/entrydsm/yapaghetti/global/config/SecurityConfig.java
@@ -1,27 +1,38 @@
-package kr.hs.entrydsm.yapaghetti.global.security;
+package kr.hs.entrydsm.yapaghetti.global.config;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import kr.hs.entrydsm.yapaghetti.global.security.jwt.JwtTokenProvider;
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 
+@RequiredArgsConstructor
+@EnableWebSecurity
 @Configuration
 public class SecurityConfig {
 
-	@Bean
-	public SecurityFilterChain filterChain(HttpSecurity http) throws  Exception {
-		http
-			.csrf().disable()
-			.formLogin().disable()
-			.sessionManagement()
-			.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
-			.and().cors();
+    private final ObjectMapper objectMapper;
+    private final JwtTokenProvider jwtTokenProvider;
 
-		http
-			.authorizeRequests()
-			.anyRequest().permitAll();
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf().disable()
+                .formLogin().disable()
+                .sessionManagement()
+                .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .and().cors();
 
-		return http.build();
-	}
+        http
+                .authorizeRequests()
+                .antMatchers("/users/auth").permitAll()
+                .anyRequest().authenticated()
+                .and().apply(new FilterConfig(objectMapper, jwtTokenProvider));
+
+        return http.build();
+    }
 }
