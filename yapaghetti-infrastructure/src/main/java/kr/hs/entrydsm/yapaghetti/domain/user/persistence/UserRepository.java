55d4daa198770d73dba0d6c9f873d8af5ebca22d
leeseojune53 <sung07288346@gmail.com>
2022-07-02 21:00:20 +0900

    ⚡️ :: User findByPublicId

--- yapaghetti-infrastructure/src/main/java/kr/hs/entrydsm/yapaghetti/domain/user/persistence/UserRepository.java
+++ yapaghetti-infrastructure/src/main/java/kr/hs/entrydsm/yapaghetti/domain/user/persistence/UserRepository.java
@@ -1,9 +1,12 @@
 package kr.hs.entrydsm.yapaghetti.domain.user.persistence;
 
 import java.util.Optional;
+import java.util.UUID;
+
 import kr.hs.entrydsm.yapaghetti.domain.user.persistence.entity.UserEntity;
 import org.springframework.data.repository.CrudRepository;
 
 public interface UserRepository extends CrudRepository<UserEntity, Long> {
 	Optional<UserEntity> findByEmail(String email);
+	Optional<UserEntity> findByPublicId(UUID publicId);
 }
