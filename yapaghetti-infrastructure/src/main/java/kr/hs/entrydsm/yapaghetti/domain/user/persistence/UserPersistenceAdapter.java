4d34bcee0127362fa44f5b85a4610c8248f1afe4
leeseojune53 <sung07288346@gmail.com>
2022-07-02 14:18:07 +0900

    ⚡️ :: Mapper 사용

--- yapaghetti-infrastructure/src/main/java/kr/hs/entrydsm/yapaghetti/domain/user/persistence/UserPersistenceAdapter.java
+++ yapaghetti-infrastructure/src/main/java/kr/hs/entrydsm/yapaghetti/domain/user/persistence/UserPersistenceAdapter.java
@@ -4,6 +4,7 @@ import java.util.Optional;
 
 import kr.hs.entrydsm.yapaghetti.domain.user.domain.User;
 import kr.hs.entrydsm.yapaghetti.domain.user.exception.UserNotFoundException;
+import kr.hs.entrydsm.yapaghetti.domain.user.mapper.UserMapper;
 import kr.hs.entrydsm.yapaghetti.domain.user.persistence.entity.UserEntity;
 import kr.hs.entrydsm.yapaghetti.domain.user.spi.QueryUserPort;
 import kr.hs.entrydsm.yapaghetti.domain.user.spi.CommandUserPort;
@@ -13,6 +14,7 @@ import lombok.RequiredArgsConstructor;
 public class UserPersistenceAdapter implements QueryUserPort, CommandUserPort {
 
     private final UserRepository userRepository;
+    private final UserMapper userMapper;
 
     public Optional<UserEntity> findUserByEmail(String email) {
         return userRepository.findByEmail(email);
@@ -22,17 +24,6 @@ public class UserPersistenceAdapter implements QueryUserPort, CommandUserPort {
     public User queryUserByEmail(String email) {
         UserEntity userEntity = userRepository.findByEmail(email)
                 .orElseThrow(() -> UserNotFoundException.EXCEPTION);
-        return User.builder()
-                .id(userEntity.getId())
-                .email(userEntity.getEmail())
-                .password(userEntity.getPassword())
-                .name(userEntity.getName())
-                .profileImagePath(userEntity.getProfileImagePath())
-                .phoneNumber(userEntity.getPhoneNumber())
-                .location(userEntity.getLocation())
-                .isVisited(userEntity.isVisited())
-                .publicId(userEntity.getPublicId())
-                .role(userEntity.getRole())
-                .build();
+        return userMapper.entityToDomain(userEntity);
     }
 }
