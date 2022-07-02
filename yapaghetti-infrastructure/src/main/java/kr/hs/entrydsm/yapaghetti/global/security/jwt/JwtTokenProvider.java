4a8032cc77ce162459922da423855a1dc78afc75
leeseojune53 <sung07288346@gmail.com>
2022-07-02 21:09:14 +0900

    ⚡️ :: getAuthentication

--- yapaghetti-infrastructure/src/main/java/kr/hs/entrydsm/yapaghetti/global/security/jwt/JwtTokenProvider.java
+++ yapaghetti-infrastructure/src/main/java/kr/hs/entrydsm/yapaghetti/global/security/jwt/JwtTokenProvider.java
@@ -11,26 +11,64 @@ import com.nimbusds.jwt.SignedJWT;
 import kr.hs.entrydsm.yapaghetti.domain.user.spi.UserJwtPort;
 import kr.hs.entrydsm.yapaghetti.global.exception.InternalServerErrorException;
 import kr.hs.entrydsm.yapaghetti.global.property.JwtProperties;
+import kr.hs.entrydsm.yapaghetti.global.security.exception.InvalidTokenException;
+import kr.hs.entrydsm.yapaghetti.global.security.exception.InvalidTokenTypeException;
+import kr.hs.entrydsm.yapaghetti.global.security.principle.AuthDetailsService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Component;
 
+import javax.servlet.http.HttpServletRequest;
+import java.text.ParseException;
 import java.util.Date;
+import java.util.UUID;
 
 @RequiredArgsConstructor
 @Component
 public class JwtTokenProvider implements UserJwtPort {
 
     private final JwtProperties jwtProperties;
+    private final AuthDetailsService authDetailsService;
+
+    public String resolveToken(HttpServletRequest request) {
+        String bearer = request.getHeader("Authorization");
+        if(bearer != null && bearer.length() > 7 && bearer.startsWith("Bearer ")) {
+            return bearer.substring(7);
+        }
+        return null;
+    }
+
+    public Authentication getAuthentication(String token) {
+        try {
+            SignedJWT signedJWT = SignedJWT.parse(token);
+            JWTClaimsSet claimsSet = signedJWT.getJWTClaimsSet();
+            String type = claimsSet.getStringClaim("type");
+            if(!type.equals("access_token")) {
+                throw InvalidTokenTypeException.EXCEPTION;
+            }
+            String subject = claimsSet.getSubject();
+
+            UserDetails userDetails = authDetailsService.loadUserByUsername(subject);
+
+            return new UsernamePasswordAuthenticationToken(userDetails, "", userDetails.getAuthorities());
+
+        } catch (ParseException e) {
+            throw InvalidTokenException.EXCEPTION;
+        }
+    }
 
     @Override
-    public String generateAccessToken(String email, String role) {
+    public String generateAccessToken(UUID publicId, String role) {
         try {
             Date expiration = getAccessExpiration();
             JWSSigner signer = new MACSigner(jwtProperties.getSecret());
 
             JWTClaimsSet claimsSet = new JWTClaimsSet.Builder()
-                    .subject(email)
+                    .subject(publicId.toString())
                     .claim("role", role)
+                    .claim("type", "access_token")
                     .expirationTime(expiration)
                     .build();
 
