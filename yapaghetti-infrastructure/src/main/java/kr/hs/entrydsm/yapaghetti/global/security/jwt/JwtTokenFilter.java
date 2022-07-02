8e6c71c267f7a1694817d5bc8a81381a57bab1fc
leeseojune53 <sung07288346@gmail.com>
2022-07-02 21:15:35 +0900

    ⚡️ :: 공백

--- yapaghetti-infrastructure/src/main/java/kr/hs/entrydsm/yapaghetti/global/security/jwt/JwtTokenFilter.java
+++ yapaghetti-infrastructure/src/main/java/kr/hs/entrydsm/yapaghetti/global/security/jwt/JwtTokenFilter.java
@@ -16,7 +16,6 @@ public class JwtTokenFilter extends OncePerRequestFilter {
 
     private final JwtTokenProvider jwtTokenProvider;
 
-
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
         String token = jwtTokenProvider.resolveToken(request);
