0c01d49d0b5e3fc23a4e05143076f57ea6450533
leeseojune53 <sung07288346@gmail.com>
2022-07-02 23:11:49 +0900

    ⚡️ :: permission

--- yapaghetti-infrastructure/src/main/java/kr/hs/entrydsm/yapaghetti/global/config/SecurityConfig.java
+++ yapaghetti-infrastructure/src/main/java/kr/hs/entrydsm/yapaghetti/global/config/SecurityConfig.java
@@ -1,6 +1,7 @@
 package kr.hs.entrydsm.yapaghetti.global.config;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import kr.hs.entrydsm.yapaghetti.domain.user.domain.UserRole;
 import kr.hs.entrydsm.yapaghetti.global.security.jwt.JwtTokenProvider;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
@@ -14,6 +15,12 @@ import org.springframework.security.web.SecurityFilterChain;
 @EnableWebSecurity
 @Configuration
 public class SecurityConfig {
+
+    private static final String STUDENT = UserRole.STUDENT.name();
+    private static final String TEACHER = UserRole.TEACHER.name();
+    private static final String COMPANY = UserRole.COMPANY.name();
+    private static final String MOU = UserRole.MOU.name();
+    private static final String NON_MOU = UserRole.NON_MOU.name();
 
     private final ObjectMapper objectMapper;
     private final JwtTokenProvider jwtTokenProvider;
@@ -30,6 +37,7 @@ public class SecurityConfig {
         http
                 .authorizeRequests()
                 .antMatchers("/users/auth").permitAll()
+                .antMatchers("/users/first-password").hasAnyRole(STUDENT, TEACHER, MOU)
                 .anyRequest().authenticated()
                 .and().apply(new FilterConfig(objectMapper, jwtTokenProvider));
 
