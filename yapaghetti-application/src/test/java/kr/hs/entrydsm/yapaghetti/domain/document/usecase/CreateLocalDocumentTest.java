185bfeaa93c02819901fb9d98935875156a8b08e
정대현 <eogus_2513@dsm.hs.kr>
2022-07-03 19:50:27 +0900

    :bookmark_tabs: :: CreateLocalDocumentTest

---
+++ yapaghetti-application/src/test/java/kr/hs/entrydsm/yapaghetti/domain/document/usecase/CreateLocalDocumentTest.java
@@ -0,0 +1,53 @@
+package kr.hs.entrydsm.yapaghetti.domain.document.usecase;
+
+import kr.hs.entrydsm.yapaghetti.domain.document.api.dto.request.DomainCreateLocalDocumentRequest;
+import kr.hs.entrydsm.yapaghetti.domain.document.spi.CommendDocumentPort;
+import kr.hs.entrydsm.yapaghetti.domain.user.domain.User;
+import kr.hs.entrydsm.yapaghetti.domain.user.spi.QueryUserPort;
+import kr.hs.entrydsm.yapaghetti.domain.user.spi.UserSecurityPort;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.UUID;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.BDDMockito.given;
+
+@ExtendWith(MockitoExtension.class)
+class CreateLocalDocumentTest {
+
+    @Mock
+    CommendDocumentPort commendDocumentPort;
+
+    @Mock
+    QueryUserPort queryUserPort;
+
+    @Mock
+    UserSecurityPort userSecurityPort;
+
+    @InjectMocks
+    CreateLocalDocumentUseCase createLocalDocumentUseCase;
+
+    @Mock
+    DomainCreateLocalDocumentRequest request;
+
+    @Test
+    void createLocalDocument() {
+        String previewImagePath = "previewImagePath";
+        String content = "content";
+
+        given(request.getPreviewImagePath())
+                .willReturn(previewImagePath);
+        given(request.getContent())
+                .willReturn(content);
+        given(userSecurityPort.getCurrentUserPublicId())
+                .willReturn(UUID.randomUUID());
+        given(queryUserPort.queryUserByPublicId(any()))
+                .willReturn(User.builder().build());
+
+        createLocalDocumentUseCase.createLocalDocument(request);
+    }
+}
