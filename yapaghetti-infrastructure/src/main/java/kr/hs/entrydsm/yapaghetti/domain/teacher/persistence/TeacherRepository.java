4d1cf5316c1aa5704ae5169494edbcfb12a9fbfb
WilterFlip <hhgnoon@naver.com>
2022-07-03 18:28:03 +0900

    ♻️ :: TeacherRepository PK

--- yapaghetti-infrastructure/src/main/java/kr/hs/entrydsm/yapaghetti/domain/teacher/persistence/TeacherRepository.java
+++ yapaghetti-infrastructure/src/main/java/kr/hs/entrydsm/yapaghetti/domain/teacher/persistence/TeacherRepository.java
@@ -3,7 +3,5 @@ package kr.hs.entrydsm.yapaghetti.domain.teacher.persistence;
 import kr.hs.entrydsm.yapaghetti.domain.teacher.persistence.entity.TeacherEntity;
 import org.springframework.data.repository.CrudRepository;
 
-import java.util.UUID;
-
-public interface TeacherRepository extends CrudRepository<TeacherEntity, UUID> {
+public interface TeacherRepository extends CrudRepository<TeacherEntity, Long> {
 }
