93bcb9bae269468c75047fdb77778da5c4d0f712
정대현 <eogus_2513@dsm.hs.kr>
2022-07-03 20:20:57 +0900

    :recycle: :: max 설정

--- yapaghetti-infrastructure/src/main/java/kr/hs/entrydsm/yapaghetti/domain/document/presentation/dto/request/WebCreateLocalDocumentRequest.java
+++ yapaghetti-infrastructure/src/main/java/kr/hs/entrydsm/yapaghetti/domain/document/presentation/dto/request/WebCreateLocalDocumentRequest.java
@@ -3,15 +3,18 @@ package kr.hs.entrydsm.yapaghetti.domain.document.presentation.dto.request;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
+import javax.validation.constraints.Max;
 import javax.validation.constraints.NotBlank;
 
 @Getter
 @NoArgsConstructor
 public class WebCreateLocalDocumentRequest {
 
+    @Max(255)
     @NotBlank
     private String previewImagePath;
 
+    @Max(65535)
     @NotBlank
     private String content;
 }
