67d376dcc8e6d54f0246e08b938a1b521db741c1
이준서 <ljchan71@gmail.com>
2022-07-03 21:32:01 +0900

    ⚡️ :: Student One to One으로 변경

--- yapaghetti-infrastructure/src/main/java/kr/hs/entrydsm/yapaghetti/domain/student/persistence/entity/StudentEntity.java
+++ yapaghetti-infrastructure/src/main/java/kr/hs/entrydsm/yapaghetti/domain/student/persistence/entity/StudentEntity.java
@@ -7,6 +7,7 @@ import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 import javax.persistence.MapsId;
+import javax.persistence.OneToOne;
 import javax.persistence.Table;
 import kr.hs.entrydsm.yapaghetti.domain.tag.persistence.entity.TagEntity;
 import kr.hs.entrydsm.yapaghetti.domain.user.persistence.entity.UserEntity;
@@ -28,7 +29,7 @@ public class StudentEntity {
 	private Long userId;
 
 	@MapsId
-	@ManyToOne(fetch = FetchType.LAZY)
+	@OneToOne(fetch = FetchType.LAZY)
 	@JoinColumn(name = "user_id")
 	private UserEntity user;
 
