b8c27ecb2294867a19e19f396bf17f28b866a954
이준서 <ljchan71@gmail.com>
2022-07-04 11:08:47 +0900

    ⚡️ :: FindTagById

--- yapaghetti-infrastructure/src/main/java/kr/hs/entrydsm/yapaghetti/domain/tag/persistence/TagPersistenceAdapter.java
+++ yapaghetti-infrastructure/src/main/java/kr/hs/entrydsm/yapaghetti/domain/tag/persistence/TagPersistenceAdapter.java
@@ -1,5 +1,8 @@
 package kr.hs.entrydsm.yapaghetti.domain.tag.persistence;
 
+import java.util.Optional;
+import java.util.UUID;
+import kr.hs.entrydsm.yapaghetti.domain.tag.persistence.entity.TagEntity;
 import lombok.RequiredArgsConstructor;
 
 @RequiredArgsConstructor
@@ -7,4 +10,7 @@ public class TagPersistenceAdapter {
 
     private final TagRepository tagRepository;
 
+    public Optional<TagEntity> findTagById(UUID tagId) {
+        return tagRepository.findById(tagId);
+    }
 }
