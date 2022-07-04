8d3d9388ef1f4fd7cea6b03db9ab75d658a9c6f2
이준서 <ljchan71@gmail.com>
2022-07-04 11:09:00 +0900

    ⚡️ :: Long to UUID

--- yapaghetti-infrastructure/src/main/java/kr/hs/entrydsm/yapaghetti/domain/document/persistence/DocumentRepository.java
+++ yapaghetti-infrastructure/src/main/java/kr/hs/entrydsm/yapaghetti/domain/document/persistence/DocumentRepository.java
@@ -1,7 +1,8 @@
 package kr.hs.entrydsm.yapaghetti.domain.document.persistence;
 
+import java.util.UUID;
 import kr.hs.entrydsm.yapaghetti.domain.document.persistence.entity.DocumentEntity;
 import org.springframework.data.repository.CrudRepository;
 
-public interface DocumentRepository extends CrudRepository<DocumentEntity, Long> {
+public interface DocumentRepository extends CrudRepository<DocumentEntity, UUID> {
 }
--- yapaghetti-infrastructure/src/main/java/kr/hs/entrydsm/yapaghetti/domain/tag/persistence/TagRepository.java
+++ yapaghetti-infrastructure/src/main/java/kr/hs/entrydsm/yapaghetti/domain/tag/persistence/TagRepository.java
@@ -1,7 +1,8 @@
 package kr.hs.entrydsm.yapaghetti.domain.tag.persistence;
 
+import java.util.UUID;
 import kr.hs.entrydsm.yapaghetti.domain.tag.persistence.entity.TagEntity;
 import org.springframework.data.repository.CrudRepository;
 
-public interface TagRepository extends CrudRepository<TagEntity, Long> {
+public interface TagRepository extends CrudRepository<TagEntity, UUID> {
 }
