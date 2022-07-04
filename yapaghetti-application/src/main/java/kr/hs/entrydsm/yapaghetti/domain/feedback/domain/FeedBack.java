7e58d1f19054918348271ac506b472900bd1608a
WilterFlip <hhgnoon@naver.com>
2022-07-04 15:24:31 +0900

    📑 :: FeedBack

---
+++ yapaghetti-application/src/main/java/kr/hs/entrydsm/yapaghetti/domain/feedback/domain/FeedBack.java
@@ -0,0 +1,22 @@
+package kr.hs.entrydsm.yapaghetti.domain.feedback.domain;
+
+import kr.hs.entrydsm.yapaghetti.annotation.Aggregate;
+import lombok.Builder;
+import lombok.Getter;
+
+import java.util.UUID;
+
+@Getter
+@Builder
+@Aggregate
+public class FeedBack {
+
+    private final Integer sequence;
+
+    private final UUID documentId;
+
+    private final String content;
+
+    private boolean isApply;
+
+}
