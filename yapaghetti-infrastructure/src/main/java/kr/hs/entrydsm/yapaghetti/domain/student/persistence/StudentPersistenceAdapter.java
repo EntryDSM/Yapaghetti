f5593587f5d5a6b39e92f899cddcdffa1b8ef195
이승윤 <yyuunn17@naver.com>
2022-07-04 23:05:39 +0900

    ♻️ :: StudentPersistenceAdapter @Adapter 어노테이션 추가

--- yapaghetti-infrastructure/src/main/java/kr/hs/entrydsm/yapaghetti/domain/student/persistence/StudentPersistenceAdapter.java
+++ yapaghetti-infrastructure/src/main/java/kr/hs/entrydsm/yapaghetti/domain/student/persistence/StudentPersistenceAdapter.java
@@ -1,10 +1,12 @@
 package kr.hs.entrydsm.yapaghetti.domain.student.persistence;
 
+import kr.hs.entrydsm.yapaghetti.global.annotation.Adapter;
 import lombok.RequiredArgsConstructor;
 
 import java.util.UUID;
 
 @RequiredArgsConstructor
+@Adapter
 public class StudentPersistenceAdapter {
 
 	private final StudentRepository studentRepository;
