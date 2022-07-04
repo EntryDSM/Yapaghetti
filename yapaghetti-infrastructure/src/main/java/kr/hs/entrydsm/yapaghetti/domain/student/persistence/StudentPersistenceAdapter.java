f5a782c5d730747c0ea01322123c0ef85d01318b
이승윤 <yyuunn17@naver.com>
2022-07-04 18:27:48 +0900

    ⚡️ :: student에서 tagId가 존재하는지 existsByTagId 추가

--- yapaghetti-infrastructure/src/main/java/kr/hs/entrydsm/yapaghetti/domain/student/persistence/StudentPersistenceAdapter.java
+++ yapaghetti-infrastructure/src/main/java/kr/hs/entrydsm/yapaghetti/domain/student/persistence/StudentPersistenceAdapter.java
@@ -1,9 +1,16 @@
-package kr.hs.entrydsm.yapaghetti.domain.student;
+package kr.hs.entrydsm.yapaghetti.domain.student.persistence;
 
 import lombok.RequiredArgsConstructor;
+
+import java.util.UUID;
 
 @RequiredArgsConstructor
 public class StudentPersistenceAdapter {
 
 	private final StudentRepository studentRepository;
+
+	public boolean existsByTagId(UUID tagId) {
+		return studentRepository.existsByTagId(tagId);
+	}
+
 }
