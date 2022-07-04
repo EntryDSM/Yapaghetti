dc79e9cec160b20db8c8549c214f1a29f55de5ea
KimBeomJin <sdpthf@naver.com>
2022-07-04 16:10:46 +0900

    📑 :: QueryProtectedDocumentUrlResponse

---
+++ yapaghetti-application/src/main/java/kr/hs/entrydsm/yapaghetti/domain/document/api/dto/response/QueryProtectedDocumentUrlResponse.java
@@ -0,0 +1,12 @@
+package kr.hs.entrydsm.yapaghetti.domain.document.api.dto.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class QueryProtectedDocumentUrlResponse {
+
+    private final String protectedUrl;
+
+}
