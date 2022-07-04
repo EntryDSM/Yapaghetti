93830ba0cb287be22133fc82114e3676daceb188
이승윤 <yyuunn17@naver.com>
2022-07-04 23:14:59 +0900

    ♻️ :: @Adapter 추가

--- yapaghetti-infrastructure/src/main/java/kr/hs/entrydsm/yapaghetti/domain/my_skill/persistence/MySkillPersistenceAdapter.java
+++ yapaghetti-infrastructure/src/main/java/kr/hs/entrydsm/yapaghetti/domain/my_skill/persistence/MySkillPersistenceAdapter.java
@@ -1,10 +1,12 @@
 package kr.hs.entrydsm.yapaghetti.domain.my_skill.persistence;
 
+import kr.hs.entrydsm.yapaghetti.global.annotation.Adapter;
 import lombok.RequiredArgsConstructor;
 
 import java.util.UUID;
 
 @RequiredArgsConstructor
+@Adapter
 public class MySkillPersistenceAdapter {
 
     private final MySkillRepository mySkillRepository;
