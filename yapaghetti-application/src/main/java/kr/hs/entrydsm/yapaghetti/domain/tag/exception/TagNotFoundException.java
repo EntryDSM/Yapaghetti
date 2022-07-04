ba186c3f3355e9027fbc4851a40e77d762ef6334
이준서 <ljchan71@gmail.com>
2022-07-04 11:39:17 +0900

    ⚡️ :: 실수 수정

--- yapaghetti-application/src/main/java/kr/hs/entrydsm/yapaghetti/domain/tag/exception/TagNotFoundException.java
+++ yapaghetti-application/src/main/java/kr/hs/entrydsm/yapaghetti/domain/tag/exception/TagNotFoundException.java
@@ -1,12 +1,12 @@
 package kr.hs.entrydsm.yapaghetti.domain.tag.exception;
 
-import kr.hs.entrydsm.yapaghetti.domain.user.error.UserErrorCode;
+import kr.hs.entrydsm.yapaghetti.domain.tag.error.TagErrorCode;
 import kr.hs.entrydsm.yapaghetti.error.YapaghettiException;
 
 public class TagNotFoundException extends YapaghettiException {
 
 	private TagNotFoundException() {
-		super(UserErrorCode.USER_NOT_FOUND);
+		super(TagErrorCode.TAG_NOT_FOUND);
 	}
 
 	public static final YapaghettiException EXCEPTION = new TagNotFoundException();
