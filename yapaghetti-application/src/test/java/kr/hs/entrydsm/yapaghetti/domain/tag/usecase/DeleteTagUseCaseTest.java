6a6ca33d3aff38c35d297f2e13c7d43cf3299a4d
이승윤 <yyuunn17@naver.com>
2022-07-04 18:30:00 +0900

    🧪 :: 태그 삭제 테스트

---
+++ yapaghetti-application/src/test/java/kr/hs/entrydsm/yapaghetti/domain/tag/usecase/DeleteTagUseCaseTest.java
@@ -0,0 +1,42 @@
+package kr.hs.entrydsm.yapaghetti.domain.tag.usecase;
+
+import kr.hs.entrydsm.yapaghetti.domain.tag.exception.TagNotFoundException;
+import kr.hs.entrydsm.yapaghetti.domain.tag.spi.CommandTagPort;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.UUID;
+
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.mockito.BDDMockito.given;
+
+@ExtendWith(MockitoExtension.class)
+public class DeleteTagUseCaseTest {
+
+    @Mock
+    CommandTagPort commandTagPort;
+
+    @InjectMocks
+    DeleteTagUseCase deleteTagUseCase;
+
+    private static final UUID tagId = UUID.randomUUID();
+
+    @Test
+    void 태그_삭제() {
+        given(commandTagPort.existsById(tagId)).willReturn(true);
+
+        deleteTagUseCase.execute(tagId);
+    }
+
+    @Test
+    void 태그_삭제_실패() {
+        given(commandTagPort.existsById(tagId)).willReturn(false);
+
+        assertThatThrownBy(() -> deleteTagUseCase.execute(tagId))
+                .isInstanceOf(TagNotFoundException.class);
+    }
+
+}
