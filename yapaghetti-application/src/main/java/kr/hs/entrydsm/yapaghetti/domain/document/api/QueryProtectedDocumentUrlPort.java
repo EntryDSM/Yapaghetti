d0213ef5a686f81db5857ba54a770507c4d99778
KimBeomJin <sdpthf@naver.com>
2022-07-04 16:10:57 +0900

    📑 :: QueryProtectedDocumentUrlPort

---
+++ yapaghetti-application/src/main/java/kr/hs/entrydsm/yapaghetti/domain/document/api/QueryProtectedDocumentUrlPort.java
@@ -0,0 +1,7 @@
+package kr.hs.entrydsm.yapaghetti.domain.document.api;
+
+import kr.hs.entrydsm.yapaghetti.domain.document.api.dto.response.QueryProtectedDocumentUrlResponse;
+
+public interface QueryProtectedDocumentUrlPort {
+    QueryProtectedDocumentUrlResponse execute();
+}
