156bbe8232a7d8d56f0454854c62c1172fd24c9b
KimBeomJin <sdpthf@naver.com>
2022-07-04 10:22:03 +0900

    ⚡️ :: 공개 문서 조회 Adapter 추가

--- yapaghetti-infrastructure/src/main/java/kr/hs/entrydsm/yapaghetti/domain/document/presentation/DocumentWebAdapter.java
+++ yapaghetti-infrastructure/src/main/java/kr/hs/entrydsm/yapaghetti/domain/document/presentation/DocumentWebAdapter.java
@@ -1,10 +1,14 @@
 package kr.hs.entrydsm.yapaghetti.domain.document.presentation;
 
 import kr.hs.entrydsm.yapaghetti.domain.document.api.CreateLocalDocumentPort;
+import kr.hs.entrydsm.yapaghetti.domain.document.api.QueryPublicDocumentPort;
 import kr.hs.entrydsm.yapaghetti.domain.document.api.dto.request.DomainCreateLocalDocumentRequest;
+import kr.hs.entrydsm.yapaghetti.domain.document.api.dto.response.QueryPublicDocumentResponse;
 import kr.hs.entrydsm.yapaghetti.domain.document.presentation.dto.request.WebCreateLocalDocumentRequest;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -12,6 +16,7 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.validation.Valid;
+import java.util.UUID;
 
 @RequiredArgsConstructor
 @RequestMapping("/documents")
@@ -19,6 +24,7 @@ import javax.validation.Valid;
 public class DocumentWebAdapter {
 
     private final CreateLocalDocumentPort createLocalDocumentPort;
+    private final QueryPublicDocumentPort queryPublicDocumentPort;
 
     @ResponseStatus(HttpStatus.CREATED)
     @PostMapping
@@ -29,5 +35,10 @@ public class DocumentWebAdapter {
                         .content(request.getContent())
                         .build()
         );
+    }
+
+    @GetMapping("/public/{document-id}")
+    public QueryPublicDocumentResponse getPublicDocument(@PathVariable("document-id") UUID documentId) {
+        return queryPublicDocumentPort.execute(documentId);
     }
 }
