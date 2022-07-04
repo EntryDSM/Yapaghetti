de360618a4ff6afd6ed929ed063bde7bd84e811c
leeseojune53 <sung07288346@gmail.com>
2022-07-05 00:51:29 +0900

    ⚡️ :: CompanyPersistenceAdapter

--- yapaghetti-infrastructure/src/main/java/kr/hs/entrydsm/yapaghetti/domain/company/persistence/CompanyPersistenceAdapter.java
+++ yapaghetti-infrastructure/src/main/java/kr/hs/entrydsm/yapaghetti/domain/company/persistence/CompanyPersistenceAdapter.java
@@ -1,10 +1,30 @@
 package kr.hs.entrydsm.yapaghetti.domain.company.persistence;
 
+import kr.hs.entrydsm.yapaghetti.domain.company.domain.Company;
+import kr.hs.entrydsm.yapaghetti.domain.company.exception.CompanyNotFoundException;
+import kr.hs.entrydsm.yapaghetti.domain.company.mapper.CompanyMapper;
+import kr.hs.entrydsm.yapaghetti.domain.company.spi.CommandCompanyPort;
+import kr.hs.entrydsm.yapaghetti.domain.company.spi.QueryCompanyPort;
 import lombok.RequiredArgsConstructor;
 
+import java.util.UUID;
+
 @RequiredArgsConstructor
-public class CompanyPersistenceAdapter {
+public class CompanyPersistenceAdapter implements QueryCompanyPort, CommandCompanyPort {
 
-	private final CompanyRepository companyRepository;
+    private final CompanyRepository companyRepository;
+    private final CompanyMapper companyMapper;
 
+    @Override
+    public Company findCompanyByUserId(UUID userId) {
+        return companyMapper.entityToDomain(
+                companyRepository.findByUserId(userId)
+                        .orElseThrow(() -> CompanyNotFoundException.EXCEPTION)
+        );
+    }
+
+    @Override
+    public void updateCompany(Company company) {
+        companyRepository.save(companyMapper.domainToEntity(company));
+    }
 }
