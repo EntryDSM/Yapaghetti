ddbb8be33eca5555ac41df0caf39234fb610bd97
이승윤 <yyuunn17@naver.com>
2022-07-05 01:13:19 +0900

    ⚡️ :: 기술 태그 설정 기능 추가

---
+++ yapaghetti-application/src/main/java/kr/hs/entrydsm/yapaghetti/domain/tag/usecase/AddMySkillUseCase.java
@@ -0,0 +1,37 @@
+package kr.hs.entrydsm.yapaghetti.domain.tag.usecase;
+
+import kr.hs.entrydsm.yapaghetti.annotation.UseCase;
+import kr.hs.entrydsm.yapaghetti.domain.my_skill.domain.MySkill;
+import kr.hs.entrydsm.yapaghetti.domain.my_skill.spi.CommandMySkillPort;
+import kr.hs.entrydsm.yapaghetti.domain.tag.api.AddMySkillPort;
+import kr.hs.entrydsm.yapaghetti.domain.tag.api.dto.request.DomainAddMySkillRequest;
+import kr.hs.entrydsm.yapaghetti.domain.user.spi.UserSecurityPort;
+import lombok.RequiredArgsConstructor;
+
+import java.util.List;
+import java.util.UUID;
+import java.util.stream.Collectors;
+
+@RequiredArgsConstructor
+@UseCase
+public class AddMySkillUseCase implements AddMySkillPort {
+
+    private final UserSecurityPort userSecurityPort;
+
+    private final CommandMySkillPort commandMySkillPort;
+
+    @Override
+    public void execute(DomainAddMySkillRequest request) {
+        UUID userId = userSecurityPort.getCurrentUserId();
+
+        List<MySkill> mySkills = request.getTagList().stream()
+                .map(tagId -> MySkill.builder()
+                                .tagId(tagId)
+                                .userId(userId)
+                                .build()
+                ).collect(Collectors.toList());
+
+        commandMySkillPort.saveAllMySkill(mySkills);
+    }
+
+}
