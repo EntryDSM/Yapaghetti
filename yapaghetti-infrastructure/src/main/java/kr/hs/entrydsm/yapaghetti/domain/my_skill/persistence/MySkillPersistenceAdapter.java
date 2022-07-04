66f6c78b7bc95430afb2923573a84cd3c214e33b
이승윤 <yyuunn17@naver.com>
2022-07-04 18:28:38 +0900

    ⚡️ :: my_skill에서 tagId가 존재하는지 existsByTagId 추가

--- yapaghetti-infrastructure/src/main/java/kr/hs/entrydsm/yapaghetti/domain/my_skill/persistence/MySkillPersistenceAdapter.java
+++ yapaghetti-infrastructure/src/main/java/kr/hs/entrydsm/yapaghetti/domain/my_skill/persistence/MySkillPersistenceAdapter.java
@@ -2,9 +2,15 @@ package kr.hs.entrydsm.yapaghetti.domain.my_skill.persistence;
 
 import lombok.RequiredArgsConstructor;
 
+import java.util.UUID;
+
 @RequiredArgsConstructor
 public class MySkillPersistenceAdapter {
 
     private final MySkillRepository mySkillRepository;
+
+    public boolean existsByTagId(UUID tagId) {
+        return mySkillRepository.existsByTagId(tagId);
+    }
 
 }
