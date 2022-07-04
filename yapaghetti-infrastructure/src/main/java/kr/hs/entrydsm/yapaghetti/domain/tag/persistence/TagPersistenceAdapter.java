5cae4d935910a2c5291b45ee9066f2ea387aee56
이준서 <ljchan71@gmail.com>
2022-07-04 11:18:19 +0900

    ⚡️ :: Adapter 어노테이션

--- yapaghetti-infrastructure/src/main/java/kr/hs/entrydsm/yapaghetti/domain/tag/persistence/TagPersistenceAdapter.java
+++ yapaghetti-infrastructure/src/main/java/kr/hs/entrydsm/yapaghetti/domain/tag/persistence/TagPersistenceAdapter.java
@@ -3,9 +3,11 @@ package kr.hs.entrydsm.yapaghetti.domain.tag.persistence;
 import java.util.Optional;
 import java.util.UUID;
 import kr.hs.entrydsm.yapaghetti.domain.tag.persistence.entity.TagEntity;
+import kr.hs.entrydsm.yapaghetti.global.annotation.Adapter;
 import lombok.RequiredArgsConstructor;
 
 @RequiredArgsConstructor
+@Adapter
 public class TagPersistenceAdapter {
 
     private final TagRepository tagRepository;
