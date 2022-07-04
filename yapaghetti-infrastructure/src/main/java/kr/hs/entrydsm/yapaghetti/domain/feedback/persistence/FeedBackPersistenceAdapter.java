7ec1c5d17f4727b876377e587e08a160f0b0ad09
WilterFlip <hhgnoon@naver.com>
2022-07-04 15:26:16 +0900

    ⚡️ :: saveFeedBack

--- yapaghetti-infrastructure/src/main/java/kr/hs/entrydsm/yapaghetti/domain/feedback/persistence/FeedBackPersistenceAdapter.java
+++ yapaghetti-infrastructure/src/main/java/kr/hs/entrydsm/yapaghetti/domain/feedback/persistence/FeedBackPersistenceAdapter.java
@@ -1,10 +1,21 @@
 package kr.hs.entrydsm.yapaghetti.domain.feedback.persistence;
 
+import kr.hs.entrydsm.yapaghetti.domain.feedback.domain.FeedBack;
+import kr.hs.entrydsm.yapaghetti.domain.feedback.mapper.FeedbackMapper;
+import kr.hs.entrydsm.yapaghetti.domain.feedback.spi.CommandFeedbackPort;
 import lombok.RequiredArgsConstructor;
 
 @RequiredArgsConstructor
-public class FeedBackPersistenceAdapter {
+public class FeedBackPersistenceAdapter implements CommandFeedbackPort {
 
     private final FeedBackRepository feedBackRepository;
 
+    private final FeedbackMapper feedbackMapper;
+
+    @Override
+    public void saveFeedback(FeedBack feedBack) {
+        feedBackRepository.save(
+                feedbackMapper.domainToEntity(feedBack)
+        );
+    }
 }
