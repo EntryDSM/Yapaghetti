39c628ce92263f443410409f674e2fdfe4fe28c4
WilterFlip <hhgnoon@naver.com>
2022-07-04 16:14:02 +0900

    ♻️ :: Adapter annotation

--- yapaghetti-infrastructure/src/main/java/kr/hs/entrydsm/yapaghetti/domain/teacher/persistence/TeacherPersistenceAdapter.java
+++ yapaghetti-infrastructure/src/main/java/kr/hs/entrydsm/yapaghetti/domain/teacher/persistence/TeacherPersistenceAdapter.java
@@ -1,8 +1,10 @@
 package kr.hs.entrydsm.yapaghetti.domain.teacher.persistence;
 
+import kr.hs.entrydsm.yapaghetti.global.annotation.Adapter;
 import lombok.RequiredArgsConstructor;
 
 @RequiredArgsConstructor
+@Adapter
 public class TeacherPersistenceAdapter {
 
     private final TeacherRepository teacherRepository;
