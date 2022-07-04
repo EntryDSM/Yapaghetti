09bbcb534e518b74cd4fdb00b1246ebf072a422c
WilterFlip <hhgnoon@naver.com>
2022-07-04 15:47:39 +0900

    📑 ::	CreateFeedbackWepAdapter

---
+++ yapaghetti-infrastructure/src/main/java/kr/hs/entrydsm/yapaghetti/domain/teacher/presentation/CreateFeedbackWepAdapter.java
@@ -0,0 +1,38 @@
+package kr.hs.entrydsm.yapaghetti.domain.teacher.presentation;
+
+import kr.hs.entrydsm.yapaghetti.domain.teacher.api.CreateFeedbackAPI;
+import kr.hs.entrydsm.yapaghetti.domain.teacher.api.dto.request.DomainCreateFeedbackRequest;
+import kr.hs.entrydsm.yapaghetti.domain.teacher.presentation.dto.WebCreateFeedbackRequest;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.validation.Valid;
+
+
+@RequiredArgsConstructor
+@RequestMapping("/teachers")
+@RestController
+public class CreateFeedbackWepAdapter {
+
+    private final CreateFeedbackAPI createFeedbackAPI;
+
+    @PostMapping("/feedback/{student-id}")
+    @ResponseStatus(HttpStatus.CREATED)
+    public void createFeedback(@PathVariable("student-id") String studentId,
+                               @RequestBody @Valid WebCreateFeedbackRequest webCreateFeedBackRequest) {
+        createFeedbackAPI.createFeedback(
+                DomainCreateFeedbackRequest
+                        .builder()
+                            .studentId(studentId)
+                            .sequence(webCreateFeedBackRequest.getSequence())
+                            .content(webCreateFeedBackRequest.getContent())
+                        .build()
+        );
+    }
+}
