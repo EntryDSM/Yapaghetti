0ded3c8ea66c1975c70ce9362598d5a31b49e077
이승윤 <yyuunn17@naver.com>
2022-07-04 12:39:16 +0900

    🧪 :: 태그 생성 단위 테스트

---
+++ yapaghetti-application/src/test/java/kr/hs/entrydsm/yapaghetti/domain/tag/usecase/AddTagUseCaseTest.java
@@ -0,0 +1,59 @@
+package kr.hs.entrydsm.yapaghetti.domain.tag.usecase;
+
+import kr.hs.entrydsm.yapaghetti.domain.tag.api.dto.request.DomainAddTagRequest;
+import kr.hs.entrydsm.yapaghetti.domain.tag.domain.TagType;
+import kr.hs.entrydsm.yapaghetti.domain.tag.exception.AlreadyExistsTagException;
+import kr.hs.entrydsm.yapaghetti.domain.tag.spi.CommandTagPort;
+import kr.hs.entrydsm.yapaghetti.domain.tag.spi.QueryTagPort;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.BDDMockito.given;
+
+@ExtendWith(MockitoExtension.class)
+public class AddTagUseCaseTest {
+
+    @Mock
+    QueryTagPort queryTagPort;
+
+    @Mock
+    CommandTagPort saveTagPort;
+
+    @InjectMocks
+    AddTagUseCase addTagUseCase;
+
+    @Mock
+    DomainAddTagRequest request;
+
+    @Test
+    void 태그_저장() {
+        String name = "프론트엔드";
+        TagType type = TagType.MAJOR;
+
+        given(request.getName()).willReturn(name);
+        given(request.getIsMajor()).willReturn(true);
+
+        given(TagType.MAJOR.equals(type) && request.getIsMajor()).willReturn(true);
+
+        given(queryTagPort.existByName(name)).willReturn(false);
+
+        addTagUseCase.execute(request);
+    }
+
+    @Test
+    void 태그_저장_중복_이름() {
+        String name = "프론트엔드";
+
+        given(request.getName()).willReturn(name);
+        given(queryTagPort.existByName(name)).willReturn(true);
+
+        assertThrows(AlreadyExistsTagException.class, () -> {
+            addTagUseCase.execute(request);
+        });
+    }
+
+}
