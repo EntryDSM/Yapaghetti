f67a151bb4a7766d1826655de85fc5b17c211be8
KimBeomJin <sdpthf@naver.com>
2022-07-04 11:47:35 +0900

    ⚡️ :: 공개 문서 복사 권한 설정

--- yapaghetti-infrastructure/src/main/java/kr/hs/entrydsm/yapaghetti/global/security/SecurityConfig.java
+++ yapaghetti-infrastructure/src/main/java/kr/hs/entrydsm/yapaghetti/global/security/SecurityConfig.java
@@ -44,6 +44,7 @@ public class SecurityConfig {
                 .antMatchers(HttpMethod.PATCH, "/users/first-password").hasAnyRole(STUDENT, TEACHER, MOU)
 
                 .antMatchers(HttpMethod.POST, "/documents").hasRole(STUDENT)
+                .antMatchers(HttpMethod.POST, "/documents/copy").hasAnyRole(STUDENT)
                 .antMatchers(HttpMethod.GET, "/documents/public/{document-id}").hasAnyRole(STUDENT, TEACHER, MOU)
 
                 .anyRequest().authenticated()
