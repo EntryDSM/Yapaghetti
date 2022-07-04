a0835e0235a4b1677454fdba340048628edc6b2f
KimBeomJin <sdpthf@naver.com>
2022-07-04 23:00:04 +0900

    🧪 :: 공개 문서 삭제 테스트

---
+++ yapaghetti-application/src/test/java/kr/hs/entrydsm/yapaghetti/domain/document/usecase/DeletePublicDocumentTest.java
@@ -0,0 +1,40 @@
+package kr.hs.entrydsm.yapaghetti.domain.document.usecase;
+
+import kr.hs.entrydsm.yapaghetti.domain.document.domain.Document;
+import kr.hs.entrydsm.yapaghetti.domain.document.domain.DocumentType;
+import kr.hs.entrydsm.yapaghetti.domain.document.spi.CommandDocumentPort;
+import kr.hs.entrydsm.yapaghetti.domain.document.spi.QueryDocumentPort;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.UUID;
+
+import static org.mockito.BDDMockito.given;
+
+@ExtendWith(MockitoExtension.class)
+public class DeletePublicDocumentTest {
+
+    @Mock
+    QueryDocumentPort queryDocumentPort;
+
+    @Mock
+    CommandDocumentPort commandDocumentPort;
+
+    @InjectMocks
+    DeletePublicDocumentUseCase deletePublicDocumentUseCase;
+
+    @Test
+    void deletePublicDocument() {
+        UUID userId = UUID.randomUUID();
+
+        given(queryDocumentPort.queryDocumentByUserIdAndType(userId, DocumentType.PUBLIC)).willReturn(
+                Document.builder()
+                        .build()
+        );
+
+        deletePublicDocumentUseCase.execute(userId);
+    }
+}
