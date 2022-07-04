3107bd0f54f034c7d5cefd69fbeb42003a77e15a
정대현 <eogus_2513@dsm.hs.kr>
2022-07-05 01:09:15 +0900

    ⚡️ :: getLocalDocument api 추가

--- yapaghetti-infrastructure/src/main/java/kr/hs/entrydsm/yapaghetti/domain/document/presentation/DocumentWebAdapter.java
+++ yapaghetti-infrastructure/src/main/java/kr/hs/entrydsm/yapaghetti/domain/document/presentation/DocumentWebAdapter.java
@@ -3,13 +3,14 @@ package kr.hs.entrydsm.yapaghetti.domain.document.presentation;
 import kr.hs.entrydsm.yapaghetti.domain.document.api.CopyPublicDocumentPort;
 import kr.hs.entrydsm.yapaghetti.domain.document.api.CreateLocalDocumentPort;
 import kr.hs.entrydsm.yapaghetti.domain.document.api.DeletePublicDocumentPort;
+import kr.hs.entrydsm.yapaghetti.domain.document.api.QueryLocalDocumentPort;
 import kr.hs.entrydsm.yapaghetti.domain.document.api.QueryProtectedDocumentUrlPort;
 import kr.hs.entrydsm.yapaghetti.domain.document.api.QueryPublicDocumentPort;
 import kr.hs.entrydsm.yapaghetti.domain.document.api.UpdateLocalDocumentPort;
 import kr.hs.entrydsm.yapaghetti.domain.document.api.dto.request.DomainCreateLocalDocumentRequest;
 import kr.hs.entrydsm.yapaghetti.domain.document.api.dto.request.DomainUpdateLocalDocumentRequest;
+import kr.hs.entrydsm.yapaghetti.domain.document.api.dto.response.QueryDocumentResponse;
 import kr.hs.entrydsm.yapaghetti.domain.document.api.dto.response.QueryProtectedDocumentUrlResponse;
-import kr.hs.entrydsm.yapaghetti.domain.document.api.dto.response.QueryPublicDocumentResponse;
 import kr.hs.entrydsm.yapaghetti.domain.document.presentation.dto.request.WebLocalDocumentRequest;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
@@ -24,6 +25,7 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.validation.Valid;
+import javax.validation.constraints.NotBlank;
 import java.util.UUID;
 
 @RequiredArgsConstructor
@@ -37,6 +39,7 @@ public class DocumentWebAdapter {
     private final UpdateLocalDocumentPort updateLocalDocumentPort;
     private final QueryProtectedDocumentUrlPort queryProtectedDocumentUrlPort;
     private final DeletePublicDocumentPort deletePublicDocumentPort;
+    private final QueryLocalDocumentPort queryLocalDocumentPort;
 
     @ResponseStatus(HttpStatus.CREATED)
     @PostMapping
@@ -50,7 +53,7 @@ public class DocumentWebAdapter {
     }
 
     @GetMapping("/public/{document-id}")
-    public QueryPublicDocumentResponse getPublicDocument(@PathVariable("document-id") UUID documentId) {
+    public QueryDocumentResponse getPublicDocument(@PathVariable("document-id") UUID documentId) {
         return queryPublicDocumentPort.execute(documentId);
     }
 
@@ -83,5 +86,10 @@ public class DocumentWebAdapter {
     @DeleteMapping("/public/{student-id}")
     public void deletePublicDocument(@PathVariable("student-id") UUID userId) {
         deletePublicDocumentPort.execute(userId);
+    }
+
+    @GetMapping("{document-id}")
+    public QueryDocumentResponse getLocalDocument(@PathVariable("document-id") @NotBlank UUID documentId) {
+        return queryLocalDocumentPort.execute(documentId);
     }
 }
