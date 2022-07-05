2dc9953c1d61959edfb1d3a7f5a09fee0d6d8828
이승윤 <yyuunn17@naver.com>
2022-07-05 12:57:05 +0900

    ♻️ :: @NotBlank 추가

--- yapaghetti-infrastructure/src/main/java/kr/hs/entrydsm/yapaghetti/domain/tag/presentation/dto/request/WebSetMajorTagRequest.java
+++ yapaghetti-infrastructure/src/main/java/kr/hs/entrydsm/yapaghetti/domain/tag/presentation/dto/request/WebSetMajorTagRequest.java
@@ -3,10 +3,14 @@ package kr.hs.entrydsm.yapaghetti.domain.tag.presentation.dto.request;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
+import javax.validation.constraints.NotBlank;
 import java.util.UUID;
 
 @Getter
 @NoArgsConstructor
 public class WebSetMajorTagRequest {
+
+    @NotBlank
     private UUID tagId;
+
 }
