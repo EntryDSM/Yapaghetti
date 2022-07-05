bcc17e1551672d6a6a36f88d7abdb4f0c40edab2
WilterFlip <hhgnoon@naver.com>
2022-07-05 10:11:44 +0900

    ♻️ ::	annotation & 줄바꿈

--- yapaghetti-application/src/main/java/kr/hs/entrydsm/yapaghetti/domain/teacher/api/CreateFeedBackPort.java
+++ yapaghetti-application/src/main/java/kr/hs/entrydsm/yapaghetti/domain/teacher/api/CreateFeedBackPort.java
@@ -1,6 +1,5 @@
 package kr.hs.entrydsm.yapaghetti.domain.teacher.api;
 
-
 import kr.hs.entrydsm.yapaghetti.domain.teacher.api.dto.request.DomainCreateFeedBackRequest;
 
 public interface CreateFeedBackPort {
--- yapaghetti-infrastructure/src/main/java/kr/hs/entrydsm/yapaghetti/domain/teacher/presentation/dto/request/WebCreateFeedBackRequest.java
+++ yapaghetti-infrastructure/src/main/java/kr/hs/entrydsm/yapaghetti/domain/teacher/presentation/dto/request/WebCreateFeedBackRequest.java
@@ -1,18 +1,11 @@
 package kr.hs.entrydsm.yapaghetti.domain.teacher.presentation.dto.request;
 
-import lombok.AccessLevel;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
 import lombok.Getter;
-import lombok.NoArgsConstructor;
 
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
 
 @Getter
-@Builder
-@NoArgsConstructor(access = AccessLevel.PROTECTED)
-@AllArgsConstructor(access = AccessLevel.PRIVATE)
 public class WebCreateFeedBackRequest {
 
     @NotNull
