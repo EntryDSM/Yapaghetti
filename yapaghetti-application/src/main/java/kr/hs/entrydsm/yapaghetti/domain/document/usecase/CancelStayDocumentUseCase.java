a02cfaac79ecb2c8ea9fa292970d028346c768c6
정대현 <eogus_2513@dsm.hs.kr>
2022-07-05 23:03:15 +0900

    :bookmark_tabs: :: CancelStayDocumentUseCase

---
+++ yapaghetti-application/src/main/java/kr/hs/entrydsm/yapaghetti/domain/document/usecase/CancelStayDocumentUseCase.java
@@ -0,0 +1,37 @@
+package kr.hs.entrydsm.yapaghetti.domain.document.usecase;
+
+import kr.hs.entrydsm.yapaghetti.annotation.UseCase;
+import kr.hs.entrydsm.yapaghetti.domain.document.api.CancelStayDocumentPort;
+import kr.hs.entrydsm.yapaghetti.domain.document.domain.Document;
+import kr.hs.entrydsm.yapaghetti.domain.document.domain.DocumentType;
+import kr.hs.entrydsm.yapaghetti.domain.document.spi.CommandDocumentPort;
+import kr.hs.entrydsm.yapaghetti.domain.document.spi.DocumentSecurityPort;
+import kr.hs.entrydsm.yapaghetti.domain.document.spi.QueryDocumentPort;
+import kr.hs.entrydsm.yapaghetti.domain.user.domain.User;
+import kr.hs.entrydsm.yapaghetti.domain.user.spi.QueryUserPort;
+import lombok.RequiredArgsConstructor;
+
+import java.util.UUID;
+
+@RequiredArgsConstructor
+@UseCase
+public class CancelStayDocumentUseCase implements CancelStayDocumentPort {
+
+    private final QueryUserPort queryUserPort;
+    private final DocumentSecurityPort documentSecurityPort;
+    private final QueryDocumentPort queryDocumentPort;
+    private final CommandDocumentPort commandDocumentPort;
+
+    @Override
+    public void execute(UUID documentId) {
+        User user = queryUserPort.queryUserById(documentSecurityPort.getCurrentUserId());
+
+        Document document = queryDocumentPort.queryDocumentByIdAndUserIdAndType(
+                documentId,
+                user.getId(),
+                DocumentType.STAY
+        );
+
+        commandDocumentPort.cancelStayDocument(document);
+    }
+}
