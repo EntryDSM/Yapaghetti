602fe685d26c6c8e58a22a0e00eb1acb3e83a518
이준서 <ljchan71@gmail.com>
2022-07-05 12:10:23 +0900

    ⚡️ :: StudentPersistenceAdapter

--- yapaghetti-infrastructure/src/main/java/kr/hs/entrydsm/yapaghetti/domain/student/persistence/StudentPersistenceAdapter.java
+++ yapaghetti-infrastructure/src/main/java/kr/hs/entrydsm/yapaghetti/domain/student/persistence/StudentPersistenceAdapter.java
@@ -1,5 +1,9 @@
 package kr.hs.entrydsm.yapaghetti.domain.student.persistence;
 
+import kr.hs.entrydsm.yapaghetti.domain.student.domain.Student;
+import kr.hs.entrydsm.yapaghetti.domain.student.exception.StudentNotFoundException;
+import kr.hs.entrydsm.yapaghetti.domain.student.mapper.StudentMapper;
+import kr.hs.entrydsm.yapaghetti.domain.student.spi.QueryStudentPort;
 import kr.hs.entrydsm.yapaghetti.global.annotation.Adapter;
 import lombok.RequiredArgsConstructor;
 
@@ -7,12 +11,20 @@ import java.util.UUID;
 
 @RequiredArgsConstructor
 @Adapter
-public class StudentPersistenceAdapter {
+public class StudentPersistenceAdapter implements QueryStudentPort {
 
 	private final StudentRepository studentRepository;
+	private final StudentMapper studentMapper;
 
 	public boolean existsByTagId(UUID tagId) {
 		return studentRepository.existsByTagId(tagId);
 	}
 
+	@Override
+	public Student queryUserById(UUID id) {
+		return studentMapper.entityToDomain(
+			studentRepository.findById(id)
+				.orElseThrow(() -> StudentNotFoundException.EXCEPTION)
+		);
+	}
 }
