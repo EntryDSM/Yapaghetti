3ca59dd13382d9465fd0d48b977a078857263d9c
WilterFlip <hhgnoon@naver.com>
2022-07-05 09:02:44 +0900

    📑 ::	CreateFeedBackPort

---
+++ yapaghetti-application/src/main/java/kr/hs/entrydsm/yapaghetti/domain/teacher/api/CreateFeedBackPort.java
@@ -0,0 +1,8 @@
+package kr.hs.entrydsm.yapaghetti.domain.teacher.api;
+
+
+import kr.hs.entrydsm.yapaghetti.domain.teacher.api.dto.request.DomainCreateFeedBackRequest;
+
+public interface CreateFeedBackPort {
+    void createFeedBack(DomainCreateFeedBackRequest domainCreateFeedBackRequest);
+}
