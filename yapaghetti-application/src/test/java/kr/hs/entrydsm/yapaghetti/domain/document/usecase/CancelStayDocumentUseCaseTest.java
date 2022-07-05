b1ccd140718cc2c0cc2216242bcd884dd34903c6
정대현 <eogus_2513@dsm.hs.kr>
2022-07-05 23:03:26 +0900

    :bookmark_tabs: :: CancelStayDocumentUseCaseTest

---
+++ yapaghetti-application/src/test/java/kr/hs/entrydsm/yapaghetti/domain/document/usecase/CancelStayDocumentUseCaseTest.java
@@ -0,0 +1,64 @@
+package kr.hs.entrydsm.yapaghetti.domain.document.usecase;
+
+import kr.hs.entrydsm.yapaghetti.domain.document.domain.Document;
+import kr.hs.entrydsm.yapaghetti.domain.document.domain.DocumentType;
+import kr.hs.entrydsm.yapaghetti.domain.document.spi.CommandDocumentPort;
+import kr.hs.entrydsm.yapaghetti.domain.document.spi.DocumentSecurityPort;
+import kr.hs.entrydsm.yapaghetti.domain.document.spi.QueryDocumentPort;
+import kr.hs.entrydsm.yapaghetti.domain.user.domain.User;
+import kr.hs.entrydsm.yapaghetti.domain.user.spi.QueryUserPort;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.UUID;
+
+import static org.mockito.BDDMockito.given;
+
+@ExtendWith(MockitoExtension.class)
+class CancelStayDocumentUseCaseTest {
+
+    @Mock
+    QueryUserPort queryUserPort;
+
+    @Mock
+    CommandDocumentPort commandDocumentPort;
+
+    @Mock
+    QueryDocumentPort queryDocumentPort;
+
+    @Mock
+    DocumentSecurityPort documentSecurityPort;
+
+    @InjectMocks
+    CancelStayDocumentUseCase cancelStayDocumentUseCase;
+
+    @Test
+    void cancelStayDocument() {
+        UUID documentId = UUID.randomUUID();
+        UUID userId = UUID.randomUUID();
+
+        given(documentSecurityPort.getCurrentUserId())
+                .willReturn(userId);
+        given(queryUserPort.queryUserById(documentSecurityPort.getCurrentUserId()))
+                .willReturn(
+                        User.builder()
+                                .id(userId)
+                                .build()
+                );
+        given(queryDocumentPort.queryDocumentByIdAndUserIdAndType(
+                documentId,
+                userId,
+                DocumentType.STAY)
+        ).willReturn(
+                        Document.builder()
+                                .id(documentId)
+                                .build()
+                );
+
+        cancelStayDocumentUseCase.execute(documentId);
+    }
+
+}
