38c5eb82ac2c6a37279e270c7f01c6a395f3812a
leeseojune53 <sung07288346@gmail.com>
2022-07-07 01:58:04 +0900

    ⚡️ :: GET /compaines/students

--- yapaghetti-infrastructure/src/main/java/kr/hs/entrydsm/yapaghetti/domain/company/presentation/CompanyWebAdapter.java
+++ yapaghetti-infrastructure/src/main/java/kr/hs/entrydsm/yapaghetti/domain/company/presentation/CompanyWebAdapter.java
@@ -1,10 +1,14 @@
 package kr.hs.entrydsm.yapaghetti.domain.company.presentation;
 
+import kr.hs.entrydsm.yapaghetti.domain.company.api.QueryStudentListPort;
 import kr.hs.entrydsm.yapaghetti.domain.company.api.UpdateCompanyNamePort;
 import kr.hs.entrydsm.yapaghetti.domain.company.api.dto.request.DomainUpdateCompanyNameRequest;
+import kr.hs.entrydsm.yapaghetti.domain.company.api.dto.request.DomainQueryStudentListRequest;
+import kr.hs.entrydsm.yapaghetti.domain.company.api.dto.response.QueryStudentListResponse;
 import kr.hs.entrydsm.yapaghetti.domain.company.presentation.dto.request.WebUpdateCompanyNameRequest;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -19,6 +23,7 @@ import javax.validation.Valid;
 public class CompanyWebAdapter {
 
     private final UpdateCompanyNamePort updateCompanyNamePort;
+    private final QueryStudentListPort queryStudentListPort;
 
     @ResponseStatus(HttpStatus.NO_CONTENT)
     @PatchMapping("/name")
@@ -26,6 +31,11 @@ public class CompanyWebAdapter {
         updateCompanyNamePort.execute(
                 new DomainUpdateCompanyNameRequest(request.getCompanyName())
         );
+    }
+
+    @GetMapping("/students")
+    public QueryStudentListResponse queryStudentList(DomainQueryStudentListRequest request) {
+        return queryStudentListPort.execute(request);
     }
 
 }
