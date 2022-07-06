8c93b4db7690f67a1dd721aa4be209d445835f29
leeseojune53 <sung07288346@gmail.com>
2022-07-07 01:42:08 +0900

    ⚡️ :: QueryStudentListPort

--- yapaghetti-application/src/main/java/kr/hs/entrydsm/yapaghetti/domain/company/api/QueryStudentListPort.java
+++ yapaghetti-application/src/main/java/kr/hs/entrydsm/yapaghetti/domain/company/api/QueryStudentListPort.java
@@ -1,7 +1,8 @@
 package kr.hs.entrydsm.yapaghetti.domain.company.api;
 
+import kr.hs.entrydsm.yapaghetti.domain.company.api.dto.request.DomainQueryStudentListRequest;
 import kr.hs.entrydsm.yapaghetti.domain.company.api.dto.response.QueryStudentListResponse;
 
 public interface QueryStudentListPort {
-    QueryStudentListResponse execute();
+    QueryStudentListResponse execute(DomainQueryStudentListRequest request);
 }
--- yapaghetti-application/src/main/java/kr/hs/entrydsm/yapaghetti/domain/company/usecase/QueryStudentListUseCase.java
+++ yapaghetti-application/src/main/java/kr/hs/entrydsm/yapaghetti/domain/company/usecase/QueryStudentListUseCase.java
@@ -2,6 +2,7 @@ package kr.hs.entrydsm.yapaghetti.domain.company.usecase;
 
 import kr.hs.entrydsm.yapaghetti.annotation.UseCase;
 import kr.hs.entrydsm.yapaghetti.domain.company.api.QueryStudentListPort;
+import kr.hs.entrydsm.yapaghetti.domain.company.api.dto.request.DomainQueryStudentListRequest;
 import kr.hs.entrydsm.yapaghetti.domain.company.api.dto.response.QueryStudentListResponse;
 import kr.hs.entrydsm.yapaghetti.domain.company.spi.CompanyQueryStudentPort;
 import lombok.RequiredArgsConstructor;
@@ -12,10 +13,13 @@ public class QueryStudentListUseCase implements QueryStudentListPort {
 
     private final CompanyQueryStudentPort companyQueryStudentPort;
 
-
     @Override
-    public QueryStudentListResponse execute() {
-        return null;
+    public QueryStudentListResponse execute(DomainQueryStudentListRequest request) {
+        return new QueryStudentListResponse(companyQueryStudentPort.findStudentByNameAndMajorAndClassNum(
+                request.getName(),
+                request.getMajor(),
+                request.getClassNum()
+        ));
     }
 
 }
