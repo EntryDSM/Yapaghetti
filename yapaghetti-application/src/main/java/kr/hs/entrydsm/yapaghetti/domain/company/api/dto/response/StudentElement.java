e77bc5e13821d3b50ce3801b411f749fdc5c7038
leeseojune53 <sung07288346@gmail.com>
2022-07-07 01:56:17 +0900

    ⚡️ :: StudentElement constructor

--- yapaghetti-application/src/main/java/kr/hs/entrydsm/yapaghetti/domain/company/api/dto/response/StudentElement.java
+++ yapaghetti-application/src/main/java/kr/hs/entrydsm/yapaghetti/domain/company/api/dto/response/StudentElement.java
@@ -1,13 +1,11 @@
 package kr.hs.entrydsm.yapaghetti.domain.company.api.dto.response;
 
-import lombok.Builder;
 import lombok.Getter;
 
 import java.util.List;
 import java.util.UUID;
 
 @Getter
-@Builder
 public class StudentElement {
 
     private final UUID studentId;
@@ -21,6 +19,16 @@ public class StudentElement {
     private final String gcn;
 
     private final String previewImagePath;
+
+    public StudentElement(UUID studentId, String major, List<String> tagList, String name, String grade, String classNum, Integer number, String previewImagePath, String profileImagePath) {
+        this.studentId = studentId;
+        this.major = major;
+        this.tagList = tagList;
+        this.name = name;
+        this.gcn = grade + classNum + String.format("%02d", number);
+        this.previewImagePath = previewImagePath;
+        this.profileImagePath = profileImagePath;
+    }
 
     private final String profileImagePath;
 
