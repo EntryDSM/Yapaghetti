6650c3ae643b0c56f42ea61374841d249df84b47
leeseojune53 <sung07288346@gmail.com>
2022-07-07 01:57:02 +0900

    ⚡️ :: StudentPersistenceAdapter querydsl

--- yapaghetti-infrastructure/src/main/java/kr/hs/entrydsm/yapaghetti/domain/student/persistence/StudentPersistenceAdapter.java
+++ yapaghetti-infrastructure/src/main/java/kr/hs/entrydsm/yapaghetti/domain/student/persistence/StudentPersistenceAdapter.java
@@ -1,25 +1,75 @@
 package kr.hs.entrydsm.yapaghetti.domain.student.persistence;
 
+import com.querydsl.core.types.Projections;
+import com.querydsl.jpa.impl.JPAQueryFactory;
 import kr.hs.entrydsm.yapaghetti.domain.company.api.dto.response.StudentElement;
+import kr.hs.entrydsm.yapaghetti.domain.document.domain.DocumentType;
 import kr.hs.entrydsm.yapaghetti.domain.student.spi.StudentPort;
+import kr.hs.entrydsm.yapaghetti.domain.tag.persistence.entity.QTagEntity;
 import kr.hs.entrydsm.yapaghetti.global.annotation.Adapter;
 import lombok.RequiredArgsConstructor;
 
 import java.util.List;
 import java.util.UUID;
 
+import static com.querydsl.core.group.GroupBy.groupBy;
+import static com.querydsl.core.group.GroupBy.list;
+import static kr.hs.entrydsm.yapaghetti.domain.document.persistence.entity.QDocumentEntity.documentEntity;
+import static kr.hs.entrydsm.yapaghetti.domain.my_skill.persistence.entity.QMySkillEntity.mySkillEntity;
+import static kr.hs.entrydsm.yapaghetti.domain.student.persistence.entity.QStudentEntity.studentEntity;
+import static kr.hs.entrydsm.yapaghetti.domain.user.persistence.entity.QUserEntity.userEntity;
+
 @RequiredArgsConstructor
 @Adapter
 public class StudentPersistenceAdapter implements StudentPort {
 
-	private final StudentRepository studentRepository;
+    private final StudentRepository studentRepository;
+    private final JPAQueryFactory jpaQueryFactory;
 
-	public boolean existsByTagId(UUID tagId) {
-		return studentRepository.existsByTagEntityId(tagId);
-	}
+    public boolean existsByTagId(UUID tagId) {
+        return studentRepository.existsByTagEntityId(tagId);
+    }
 
-	@Override
-	public List<StudentElement> findStudentByNameAndMajorAndClassNum(String name, String major, String classNum) {
-		return null;
-	}
+    @Override
+    public List<StudentElement> findStudentByNameAndMajorAndClassNum(String name, String major, String classNum) {
+        QTagEntity majorTag = new QTagEntity("majorTag");
+        QTagEntity skillTag = new QTagEntity("skillTag");
+
+        return jpaQueryFactory
+                .from(studentEntity)
+                .leftJoin(studentEntity.userEntity, userEntity)
+                .leftJoin(studentEntity.tagEntity, majorTag)
+                .leftJoin(studentEntity.documentList, documentEntity)
+                .leftJoin(studentEntity.mySkillList, mySkillEntity)
+                .leftJoin(mySkillEntity.tagEntity, skillTag)
+                .where(documentEntity.type.eq(DocumentType.PUBLIC))
+                .where(
+                        studentEntity.classNum.like(likePreProcessing(classNum))
+                                .and(userEntity.name.like(likePreProcessing(name)))
+                                .and(majorTag.name.like(likePreProcessing(major)))
+                )
+                .transform(
+                        groupBy(studentEntity.userId)
+                                .list(
+                                        Projections.constructor(
+                                                StudentElement.class,
+                                                studentEntity.userId,
+                                                majorTag.name,
+                                                list(skillTag.name),
+                                                userEntity.name,
+                                                studentEntity.grade.stringValue(),
+                                                studentEntity.classNum.stringValue(),
+                                                studentEntity.number,
+                                                documentEntity.previewImagePath,
+                                                userEntity.profileImagePath
+                                        )
+                                )
+                );
+
+    }
+
+    private String likePreProcessing(String value) {
+        return "%" + value + "%";
+    }
+
 }
