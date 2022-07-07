d079fba7c854f0dc871db8b5ebbaa6d2a870913f
KimBeomJin <sdpthf@naver.com>
2022-07-07 17:39:18 +0900

    ♻️ :: FeedBackEntity -> FeedbackEntity

--- yapaghetti-infrastructure/src/main/java/kr/hs/entrydsm/yapaghetti/domain/feedback/persistence/FeedbackRepository.java
+++ yapaghetti-infrastructure/src/main/java/kr/hs/entrydsm/yapaghetti/domain/feedback/persistence/FeedbackRepository.java
@@ -1,14 +1,14 @@
 package kr.hs.entrydsm.yapaghetti.domain.feedback.persistence;
 
-import kr.hs.entrydsm.yapaghetti.domain.feedback.persistence.entity.FeedBackEntity;
-import kr.hs.entrydsm.yapaghetti.domain.feedback.persistence.entity.FeedBackEntityId;
+import kr.hs.entrydsm.yapaghetti.domain.feedback.persistence.entity.FeedbackEntity;
+import kr.hs.entrydsm.yapaghetti.domain.feedback.persistence.entity.FeedbackEntityId;
 import org.springframework.data.repository.CrudRepository;
 
 import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
-public interface FeedbackRepository extends CrudRepository<FeedBackEntity, FeedBackEntityId> {
-    Optional<FeedBackEntity> findByIdDocumentIdAndIdSequence(UUID id_documentId, Integer id_sequence);
-	List<FeedBackEntity> findByIdDocumentId(UUID id_documentId);
+public interface FeedbackRepository extends CrudRepository<FeedbackEntity, FeedbackEntityId> {
+    Optional<FeedbackEntity> findByIdDocumentIdAndIdSequence(UUID id_documentId, Integer id_sequence);
+	List<FeedbackEntity> findByIdDocumentId(UUID id_documentId);
 }
