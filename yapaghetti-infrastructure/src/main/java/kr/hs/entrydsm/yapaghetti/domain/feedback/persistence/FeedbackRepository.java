42162fba05d7a682e3ef513d9ef49ace1cf9fe51
KimBeomJin <sdpthf@naver.com>
2022-07-07 17:36:39 +0900

    ⚡️ :: findByIdDocumentId

--- yapaghetti-infrastructure/src/main/java/kr/hs/entrydsm/yapaghetti/domain/feedback/persistence/FeedbackRepository.java
+++ yapaghetti-infrastructure/src/main/java/kr/hs/entrydsm/yapaghetti/domain/feedback/persistence/FeedbackRepository.java
@@ -1,12 +1,14 @@
 package kr.hs.entrydsm.yapaghetti.domain.feedback.persistence;
 
-import java.util.Optional;
-import java.util.UUID;
 import kr.hs.entrydsm.yapaghetti.domain.feedback.persistence.entity.FeedBackEntity;
 import kr.hs.entrydsm.yapaghetti.domain.feedback.persistence.entity.FeedBackEntityId;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
 public interface FeedbackRepository extends CrudRepository<FeedBackEntity, FeedBackEntityId> {
-	Optional<FeedBackEntity> findByIdDocumentIdAndIdSequence(UUID id_documentId,
-		Integer id_sequence);
+    Optional<FeedBackEntity> findByIdDocumentIdAndIdSequence(UUID id_documentId, Integer id_sequence);
+	List<FeedBackEntity> findByIdDocumentId(UUID id_documentId);
 }
