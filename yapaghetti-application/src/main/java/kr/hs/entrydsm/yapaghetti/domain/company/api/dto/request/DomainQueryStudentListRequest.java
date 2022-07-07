539f4b9ceb424b8ae0190cf35b8d615f8ab0f6e2
leeseojune53 <sung07288346@gmail.com>
2022-07-07 13:41:40 +0900

    ⚡️ :: Request dto constructor

--- yapaghetti-application/src/main/java/kr/hs/entrydsm/yapaghetti/domain/company/api/dto/request/DomainQueryStudentListRequest.java
+++ yapaghetti-application/src/main/java/kr/hs/entrydsm/yapaghetti/domain/company/api/dto/request/DomainQueryStudentListRequest.java
@@ -1,14 +1,23 @@
 package kr.hs.entrydsm.yapaghetti.domain.company.api.dto.request;
 
 import lombok.Getter;
-import lombok.Setter;
 
 @Getter
-@Setter
 public class DomainQueryStudentListRequest {
 
-    private String name = "%";
-    private String major = "%";
-    private String classNum = "%";
+    private final String name;
+    private final String major;
+    private final String classNum;
+
+    public DomainQueryStudentListRequest(String name, String major, String classNum) {
+        this.name = nullCheck(name);
+        this.major = nullCheck(major);
+        this.classNum = nullCheck(classNum);
+    }
+
+    private String nullCheck(String value) {
+        return value != null ? value : "%";
+    }
+
 
 }
