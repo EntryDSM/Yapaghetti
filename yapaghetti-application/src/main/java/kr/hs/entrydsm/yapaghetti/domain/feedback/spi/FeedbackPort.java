a541b4d418f43c707bb55e1c017dd95fddccce3c
KimBeomJin <sdpthf@naver.com>
2022-07-07 17:36:28 +0900

    ⚡️ :: queryFeedbackByDocumentId

--- yapaghetti-application/src/main/java/kr/hs/entrydsm/yapaghetti/domain/feedback/spi/FeedbackPort.java
+++ yapaghetti-application/src/main/java/kr/hs/entrydsm/yapaghetti/domain/feedback/spi/FeedbackPort.java
@@ -1,6 +1,7 @@
 package kr.hs.entrydsm.yapaghetti.domain.feedback.spi;
 
+import kr.hs.entrydsm.yapaghetti.domain.document.spi.DocumentQueryFeedbackPort;
 import kr.hs.entrydsm.yapaghetti.domain.student.spi.StudentQueryFeedbackPort;
 
-public interface FeedbackPort extends CommandFeedbackPort, StudentQueryFeedbackPort {
+public interface FeedbackPort extends CommandFeedbackPort, StudentQueryFeedbackPort, DocumentQueryFeedbackPort {
 }
