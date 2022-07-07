e37f9a8a281cf5cbb4a6ab73e552599266f70e76
KimBeomJin <sdpthf@naver.com>
2022-07-07 17:37:11 +0900

    🧪 :: usecase에 따라 테스트 코드 변경

--- yapaghetti-application/src/test/java/kr/hs/entrydsm/yapaghetti/domain/document/usecase/QueryStayDocumentTest.java
+++ yapaghetti-application/src/test/java/kr/hs/entrydsm/yapaghetti/domain/document/usecase/QueryStayDocumentTest.java
@@ -1,14 +1,18 @@
 package kr.hs.entrydsm.yapaghetti.domain.document.usecase;
 
+import kr.hs.entrydsm.yapaghetti.domain.document.api.dto.response.QueryFeedBackResponse;
 import kr.hs.entrydsm.yapaghetti.domain.document.domain.Document;
 import kr.hs.entrydsm.yapaghetti.domain.document.domain.DocumentType;
+import kr.hs.entrydsm.yapaghetti.domain.document.spi.DocumentQueryFeedbackPort;
 import kr.hs.entrydsm.yapaghetti.domain.document.spi.QueryDocumentPort;
+import kr.hs.entrydsm.yapaghetti.domain.feedback.domain.Feedback;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
+import java.util.List;
 import java.util.UUID;
 
 import static org.mockito.BDDMockito.given;
@@ -18,6 +22,9 @@ class QueryStayDocumentTest {
 
     @Mock
     QueryDocumentPort queryDocumentPort;
+
+    @Mock
+    DocumentQueryFeedbackPort queryFeedbackPort;
 
     @InjectMocks
     QueryStayDocumentUseCase queryStayDocumentUseCase;
@@ -30,6 +37,19 @@ class QueryStayDocumentTest {
                 Document.builder()
                         .content("testContent")
                         .build()
+        );
+
+        given(queryFeedbackPort.queryFeedbackByDocumentId(documentId)).willReturn(
+                List.of(
+                        Feedback.builder()
+                                .sequence(1)
+                                .comment("testComment")
+                                .build(),
+                        Feedback.builder()
+                                .sequence(2)
+                                .comment("testComment2")
+                                .build()
+                )
         );
 
         queryStayDocumentUseCase.execute(documentId);
