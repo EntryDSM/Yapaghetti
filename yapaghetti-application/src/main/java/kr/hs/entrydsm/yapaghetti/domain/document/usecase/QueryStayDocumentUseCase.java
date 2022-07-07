c1cd9317f50a1b171b2c81d50c9e85d21f1be5c8
KimBeomJin <sdpthf@naver.com>
2022-07-07 17:36:58 +0900

    ⚡️ :: Todo feedback 조회 적용

--- yapaghetti-application/src/main/java/kr/hs/entrydsm/yapaghetti/domain/document/usecase/QueryStayDocumentUseCase.java
+++ yapaghetti-application/src/main/java/kr/hs/entrydsm/yapaghetti/domain/document/usecase/QueryStayDocumentUseCase.java
@@ -6,24 +6,29 @@ import kr.hs.entrydsm.yapaghetti.domain.document.api.dto.response.QueryFeedBackR
 import kr.hs.entrydsm.yapaghetti.domain.document.api.dto.response.QueryStayDocumentResponse;
 import kr.hs.entrydsm.yapaghetti.domain.document.domain.Document;
 import kr.hs.entrydsm.yapaghetti.domain.document.domain.DocumentType;
+import kr.hs.entrydsm.yapaghetti.domain.document.spi.DocumentQueryFeedbackPort;
 import kr.hs.entrydsm.yapaghetti.domain.document.spi.QueryDocumentPort;
 import lombok.RequiredArgsConstructor;
 
-import java.util.ArrayList;
 import java.util.List;
 import java.util.UUID;
+import java.util.stream.Collectors;
 
 @RequiredArgsConstructor
 @UseCase
 public class QueryStayDocumentUseCase implements QueryStayDocumentPort {
 
     private final QueryDocumentPort queryDocumentPort;
+    private final DocumentQueryFeedbackPort queryFeedbackPort;
 
     @Override
     public QueryStayDocumentResponse execute(UUID documentId) {
         Document stayDocument = queryDocumentPort.queryDocumentByIdAndType(documentId, DocumentType.STAY);
 
-        List<QueryFeedBackResponse> feedBackList = new ArrayList<>(); // TODO feedback 조회
+        List<QueryFeedBackResponse> feedBackList = queryFeedbackPort.queryFeedbackByDocumentId(documentId)
+                .stream()
+                .map(f -> new QueryFeedBackResponse(f.getSequence(), f.getComment()))
+                .collect(Collectors.toList());
 
         return new QueryStayDocumentResponse(
                 feedBackList,
