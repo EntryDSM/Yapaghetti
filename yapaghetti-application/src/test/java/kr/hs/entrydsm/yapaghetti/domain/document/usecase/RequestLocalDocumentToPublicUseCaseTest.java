e4a7d75f3b9bf8c99316e2311c4f9ff8e00a270d
정대현 <eogus_2513@dsm.hs.kr>
2022-07-07 17:01:24 +0900

    :bookmark_tabs: :: RequestLocalDocumentToPublicUseCaseTest

---
+++ yapaghetti-application/src/test/java/kr/hs/entrydsm/yapaghetti/domain/document/usecase/RequestLocalDocumentToPublicUseCaseTest.java
@@ -0,0 +1,61 @@
+package kr.hs.entrydsm.yapaghetti.domain.document.usecase;
+
+import kr.hs.entrydsm.yapaghetti.domain.document.domain.Document;
+import kr.hs.entrydsm.yapaghetti.domain.document.domain.DocumentType;
+import kr.hs.entrydsm.yapaghetti.domain.document.spi.CommandDocumentPort;
+import kr.hs.entrydsm.yapaghetti.domain.document.spi.DocumentQueryUserPort;
+import kr.hs.entrydsm.yapaghetti.domain.document.spi.DocumentSecurityPort;
+import kr.hs.entrydsm.yapaghetti.domain.document.spi.QueryDocumentPort;
+import kr.hs.entrydsm.yapaghetti.domain.user.domain.User;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.UUID;
+
+import static org.mockito.BDDMockito.given;
+
+@ExtendWith(MockitoExtension.class)
+class RequestLocalDocumentToPublicUseCaseTest {
+
+    @Mock
+    DocumentQueryUserPort documentQueryUserPort;
+
+    @Mock
+    DocumentSecurityPort documentSecurityPort;
+
+    @Mock
+    QueryDocumentPort queryDocumentPort;
+
+    @Mock
+    CommandDocumentPort commandDocumentPort;
+
+    @InjectMocks
+    RequestLocalDocumentToPublicUseCase requestLocalDocumentToPublicUseCase;
+
+    @Test
+    void requestLocalDocumentToPublic() {
+        UUID userId = UUID.randomUUID();
+        UUID documentId = UUID.randomUUID();
+
+        given(documentSecurityPort.getCurrentUserId())
+                .willReturn(userId);
+        given(documentQueryUserPort.queryUserById(documentSecurityPort.getCurrentUserId()))
+                .willReturn(
+                        User.builder()
+                                .id(userId)
+                                .build()
+                );
+        given(queryDocumentPort
+                .queryDocumentByIdAndUserIdAndType(documentId, userId, DocumentType.LOCAL))
+                .willReturn(
+                        Document.builder()
+                                .id(documentId)
+                                .build()
+                );
+
+        requestLocalDocumentToPublicUseCase.execute(documentId);
+    }
+}
