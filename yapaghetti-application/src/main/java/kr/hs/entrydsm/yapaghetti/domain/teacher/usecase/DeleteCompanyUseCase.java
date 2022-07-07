9e640090c764b4997d1055dd875b8727dc1bcbd2
osangu <hhgnoon@naver.com>
2022-07-07 18:48:21 +0900

    📑 ::	DeleteCompanyUseCase

---
+++ yapaghetti-application/src/main/java/kr/hs/entrydsm/yapaghetti/domain/teacher/usecase/DeleteCompanyUseCase.java
@@ -0,0 +1,28 @@
+package kr.hs.entrydsm.yapaghetti.domain.teacher.usecase;
+
+import kr.hs.entrydsm.yapaghetti.annotation.UseCase;
+import kr.hs.entrydsm.yapaghetti.domain.company.domain.Company;
+import kr.hs.entrydsm.yapaghetti.domain.company.spi.CommandCompanyPort;
+import kr.hs.entrydsm.yapaghetti.domain.teacher.api.DeleteCompanyPort;
+import kr.hs.entrydsm.yapaghetti.domain.teacher.spi.TeacherCommandCompanyPort;
+import kr.hs.entrydsm.yapaghetti.domain.teacher.spi.TeacherQueryCompanyPort;
+import lombok.RequiredArgsConstructor;
+
+import java.util.UUID;
+
+@RequiredArgsConstructor
+@UseCase
+public class DeleteCompanyUseCase implements DeleteCompanyPort {
+
+    private final TeacherCommandCompanyPort teacherCommandCompanyPort;
+    private final TeacherQueryCompanyPort teacherQueryCompanyPort;
+
+    @Override
+    public void execute(UUID companyId) {
+        Company company = teacherQueryCompanyPort.queryCompanyByUserId(companyId);
+
+        teacherCommandCompanyPort.deleteCompany(company);
+    }
+
+
+}
