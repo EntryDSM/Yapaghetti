b3aa3708831d606671c3723120385ae1005f8273
osangu <hhgnoon@naver.com>
2022-07-08 15:44:58 +0900

    ⚡️ ::  findByIdAndType

--- yapaghetti-infrastructure/src/main/java/kr/hs/entrydsm/yapaghetti/domain/tag/persistence/TagRepository.java
+++ yapaghetti-infrastructure/src/main/java/kr/hs/entrydsm/yapaghetti/domain/tag/persistence/TagRepository.java
@@ -4,10 +4,14 @@ import kr.hs.entrydsm.yapaghetti.domain.tag.domain.TagType;
 import kr.hs.entrydsm.yapaghetti.domain.tag.persistence.entity.TagEntity;
 import org.springframework.data.repository.CrudRepository;
 
+import javax.swing.text.html.Option;
 import java.util.List;
+import java.util.Optional;
 import java.util.UUID;
 
 public interface TagRepository extends CrudRepository<TagEntity, UUID> {
     boolean existsByName(String name);
     List<TagEntity> findAllByNameContainingAndType(String name, TagType type);
+
+    Optional<TagEntity> findByIdAndType(UUID id, TagType type);
 }
