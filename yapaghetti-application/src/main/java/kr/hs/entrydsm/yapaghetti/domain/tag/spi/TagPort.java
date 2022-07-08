4ae89efe76295ce8c5f65a9310c1d134a8fb32c4
osangu <hhgnoon@naver.com>
2022-07-08 15:47:25 +0900

    ⚡️ :: extends

--- yapaghetti-application/src/main/java/kr/hs/entrydsm/yapaghetti/domain/tag/spi/TagPort.java
+++ yapaghetti-application/src/main/java/kr/hs/entrydsm/yapaghetti/domain/tag/spi/TagPort.java
@@ -1,6 +1,7 @@
 package kr.hs.entrydsm.yapaghetti.domain.tag.spi;
 
 import kr.hs.entrydsm.yapaghetti.domain.student.spi.StudentQueryTagPort;
+import kr.hs.entrydsm.yapaghetti.domain.teacher.spi.TeacherQueryTagPort;
 
-public interface TagPort extends CommandTagPort, QueryTagPort, StudentQueryTagPort {
+public interface TagPort extends CommandTagPort, QueryTagPort, StudentQueryTagPort, TeacherQueryTagPort {
 }
