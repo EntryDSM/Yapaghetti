b687f7313e1de26b92b8a6ee5f546da4d684207d
정대현 <eogus_2513@dsm.hs.kr>
2022-07-08 14:42:16 +0900

    :bookmark_tabs: :: QueryLocalDocumentListUseCaseTest

---
+++ yapaghetti-application/src/test/java/kr/hs/entrydsm/yapaghetti/domain/document/usecase/QueryLocalDocumentListUseCaseTest.java
@@ -0,0 +1,64 @@
+package kr.hs.entrydsm.yapaghetti.domain.document.usecase;
+
+import kr.hs.entrydsm.yapaghetti.domain.document.spi.DocumentQueryStudentPort;
+import kr.hs.entrydsm.yapaghetti.domain.document.spi.DocumentQueryUserPort;
+import kr.hs.entrydsm.yapaghetti.domain.document.spi.DocumentSecurityPort;
+import kr.hs.entrydsm.yapaghetti.domain.document.spi.QueryDocumentPort;
+import kr.hs.entrydsm.yapaghetti.domain.tag.domain.Tag;
+import kr.hs.entrydsm.yapaghetti.domain.user.domain.User;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.UUID;
+
+import static org.mockito.BDDMockito.given;
+
+@ExtendWith(MockitoExtension.class)
+class QueryLocalDocumentListUseCaseTest {
+
+    @Mock
+    DocumentQueryUserPort documentQueryUserPort;
+
+    @Mock
+    DocumentSecurityPort documentSecurityPort;
+
+    @Mock
+    DocumentQueryStudentPort documentQueryStudentPort;
+
+    @Mock
+    QueryDocumentPort queryDocumentPort;
+
+    @InjectMocks
+    QueryLocalDocumentListUseCase queryLocalDocumentListUseCase;
+
+    @Test
+    void queryLocalDocumentList() {
+        UUID userId = UUID.randomUUID();
+        String userName = "testUserName";
+        UUID tagId = UUID.randomUUID();
+        String tagName = "testTagName";
+
+        given(documentSecurityPort.getCurrentUserId())
+                .willReturn(userId);
+        given(documentQueryUserPort.queryUserById(documentSecurityPort.getCurrentUserId()))
+                .willReturn(
+                        User.builder()
+                                .id(userId)
+                                .name(userName)
+                                .build()
+                );
+        given(documentQueryStudentPort.queryTagByStudentId(userId))
+                .willReturn(
+                        Tag.builder()
+                                .id(tagId)
+                                .name(tagName)
+                                .build()
+                );
+
+        queryLocalDocumentListUseCase.execute();
+    }
+
+}
