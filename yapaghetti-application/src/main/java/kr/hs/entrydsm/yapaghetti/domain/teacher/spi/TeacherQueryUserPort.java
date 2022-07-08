0bbf14c0f362c4672fc5869690673044d56481fe
osangu <hhgnoon@naver.com>
2022-07-08 15:43:15 +0900

    📑 :: TeacherQueryUserPort

---
+++ yapaghetti-application/src/main/java/kr/hs/entrydsm/yapaghetti/domain/teacher/spi/TeacherQueryUserPort.java
@@ -0,0 +1,9 @@
+package kr.hs.entrydsm.yapaghetti.domain.teacher.spi;
+
+import kr.hs.entrydsm.yapaghetti.domain.user.domain.User;
+
+import java.util.UUID;
+
+public interface TeacherQueryUserPort {
+    User queryUserById(UUID userId);
+}
