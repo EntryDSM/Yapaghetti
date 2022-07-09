19ac3d9df7810d70a020b7b3103772d5d832be13
이준서 <ljchan71@gmail.com>
2022-07-09 19:19:48 +0900

    📑 :: AuthCodeLimitEntity

---
+++ yapaghetti-infrastructure/src/main/java/kr/hs/entrydsm/yapaghetti/domain/auth/persistence/entity/AuthCodeLimitEntity.java
@@ -0,0 +1,27 @@
+package kr.hs.entrydsm.yapaghetti.domain.auth.persistence.entity;
+
+import kr.hs.entrydsm.yapaghetti.domain.auth.domain.AuthCodeType;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.redis.core.RedisHash;
+
+@Getter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@RedisHash
+public class AuthCodeLimitEntity {
+
+	@Id
+	private String value;
+
+	private AuthCodeType type;
+
+	private long timeToLive;
+
+	private int count;
+}
