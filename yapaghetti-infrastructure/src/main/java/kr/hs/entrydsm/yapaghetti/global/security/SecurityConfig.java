9cc14e7640bd418026b84859a02de1ce18f31954
정대현 <eogus_2513@dsm.hs.kr>
2022-07-09 18:58:21 +0900

    ⚡️ :: antMatchers(HttpMethod.GET, "/documents/preview/{student-id}").hasAnyRole(STUDENT, TEACHER) 추가

--- yapaghetti-infrastructure/src/main/java/kr/hs/entrydsm/yapaghetti/global/security/SecurityConfig.java
+++ yapaghetti-infrastructure/src/main/java/kr/hs/entrydsm/yapaghetti/global/security/SecurityConfig.java
@@ -73,6 +73,7 @@ public class SecurityConfig {
                 .antMatchers(HttpMethod.GET, "/documents/protected-url").hasRole(STUDENT)
                 .antMatchers(HttpMethod.GET, "/documents//stay/{document-id}").hasAnyRole(STUDENT, TEACHER)
                 .antMatchers(HttpMethod.GET, "/documents/list").hasRole(STUDENT)
+                .antMatchers(HttpMethod.GET, "/documents/preview/{student-id}").hasAnyRole(STUDENT, TEACHER)
 
                 // students
                 .antMatchers(HttpMethod.GET, "/students").hasRole(STUDENT)
