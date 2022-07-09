f68b098c5db21d30f1694a9bf5e09fd3b0a98b5e
KimBeomJin <sdpthf@naver.com>
2022-07-09 19:22:02 +0900

    🧪 :: isApproved 추가

--- yapaghetti-application/src/test/java/kr/hs/entrydsm/yapaghetti/domain/document/usecase/ApproveStayDocumentTest.java
+++ yapaghetti-application/src/test/java/kr/hs/entrydsm/yapaghetti/domain/document/usecase/ApproveStayDocumentTest.java
@@ -38,6 +38,7 @@ class ApproveStayDocumentTest {
                         .content("testContent")
                         .type(DocumentType.STAY)
                         .userId(userId)
+                        .isApproved(false)
                         .build()
         );
 
