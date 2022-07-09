1e5c72037c70de46dcf4d4fe9b50fd35f01c248e
KimBeomJin <sdpthf@naver.com>
2022-07-09 18:14:24 +0900

    📑 :: ApproveStayDocumentUseCase

---
+++ yapaghetti-application/src/main/java/kr/hs/entrydsm/yapaghetti/domain/document/usecase/ApproveStayDocumentUseCase.java
@@ -0,0 +1,28 @@
+package kr.hs.entrydsm.yapaghetti.domain.document.usecase;
+
+import kr.hs.entrydsm.yapaghetti.annotation.UseCase;
+import kr.hs.entrydsm.yapaghetti.domain.document.api.ApproveStayDocumentPort;
+import kr.hs.entrydsm.yapaghetti.domain.document.domain.Document;
+import kr.hs.entrydsm.yapaghetti.domain.document.domain.DocumentType;
+import kr.hs.entrydsm.yapaghetti.domain.document.spi.CommandDocumentPort;
+import kr.hs.entrydsm.yapaghetti.domain.document.spi.QueryDocumentPort;
+import lombok.RequiredArgsConstructor;
+
+import java.util.UUID;
+
+@RequiredArgsConstructor
+@UseCase
+public class ApproveStayDocumentUseCase implements ApproveStayDocumentPort {
+
+    private final QueryDocumentPort queryDocumentPort;
+    private final CommandDocumentPort commandDocumentPort;
+
+    @Override
+    public void execute(UUID documentId) {
+        Document stayDocument = queryDocumentPort.queryDocumentByIdAndType(documentId, DocumentType.STAY);
+
+        commandDocumentPort.saveDocument(
+                stayDocument.changeDocumentType(DocumentType.PUBLIC)
+        );
+    }
+}
