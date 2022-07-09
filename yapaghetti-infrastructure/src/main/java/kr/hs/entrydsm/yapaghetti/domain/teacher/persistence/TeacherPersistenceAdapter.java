97deea380a0c4cd7e16f656fceb39be6694bba0c
osangu <hhgnoon@naver.com>
2022-07-09 18:13:51 +0900

    ♻️ :: adapter 추가

--- yapaghetti-infrastructure/src/main/java/kr/hs/entrydsm/yapaghetti/domain/teacher/persistence/TeacherPersistenceAdapter.java
+++ yapaghetti-infrastructure/src/main/java/kr/hs/entrydsm/yapaghetti/domain/teacher/persistence/TeacherPersistenceAdapter.java
@@ -1,9 +1,11 @@
 package kr.hs.entrydsm.yapaghetti.domain.teacher.persistence;
 
 import kr.hs.entrydsm.yapaghetti.domain.teacher.spi.TeacherPort;
+import kr.hs.entrydsm.yapaghetti.global.annotation.Adapter;
 import lombok.RequiredArgsConstructor;
 
 @RequiredArgsConstructor
+@Adapter
 public class TeacherPersistenceAdapter implements TeacherPort {
 
     private final TeacherRepository teacherRepository;
