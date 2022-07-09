05aec5c487c62c5fa58c94c2cf2f973deefa71e5
KimBeomJin <sdpthf@naver.com>
2022-07-09 13:42:28 +0900

    📑 :: RejectStayDocumentUseCase

---
+++ yapaghetti-application/src/main/java/kr/hs/entrydsm/yapaghetti/domain/document/usecase/RejectStayDocumentUseCase.java
@@ -0,0 +1,26 @@
+package kr.hs.entrydsm.yapaghetti.domain.document.usecase;
+
+import kr.hs.entrydsm.yapaghetti.annotation.UseCase;
+import kr.hs.entrydsm.yapaghetti.domain.document.api.RejectStayDocumentPort;
+import kr.hs.entrydsm.yapaghetti.domain.document.domain.Document;
+import kr.hs.entrydsm.yapaghetti.domain.document.domain.DocumentType;
+import kr.hs.entrydsm.yapaghetti.domain.document.spi.CommandDocumentPort;
+import kr.hs.entrydsm.yapaghetti.domain.document.spi.QueryDocumentPort;
+import lombok.RequiredArgsConstructor;
+
+import java.util.UUID;
+
+@RequiredArgsConstructor
+@UseCase
+public class RejectStayDocumentUseCase implements RejectStayDocumentPort {
+
+    private final QueryDocumentPort queryDocumentPort;
+    private final CommandDocumentPort commandDocumentPort;
+
+    @Override
+    public void execute(UUID documentId) {
+        Document stayDocument = queryDocumentPort.queryDocumentByIdAndType(documentId, DocumentType.STAY);
+
+        // TODO 거절 비즈니스 로직 추가
+    }
+}
