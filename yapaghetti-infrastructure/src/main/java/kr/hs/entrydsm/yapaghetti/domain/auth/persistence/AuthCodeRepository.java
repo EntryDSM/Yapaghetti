5f0545cf019cbcb30680bfd3ca28cfe9c1d137d4
이준서 <ljchan71@gmail.com>
2022-07-09 19:20:07 +0900

    📑 :: AuthCodeRepository

---
+++ yapaghetti-infrastructure/src/main/java/kr/hs/entrydsm/yapaghetti/domain/auth/persistence/AuthCodeRepository.java
@@ -0,0 +1,8 @@
+package kr.hs.entrydsm.yapaghetti.domain.auth.persistence;
+
+import kr.hs.entrydsm.yapaghetti.domain.auth.persistence.entity.AuthCodeEntity;
+import org.springframework.data.repository.CrudRepository;
+
+public interface AuthCodeRepository extends CrudRepository<AuthCodeEntity, String> {
+
+}
