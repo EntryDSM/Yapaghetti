7370f7243f72f7d32cb672e9421abcf6d3d73736
osangu <hhgnoon@naver.com>
2022-07-10 02:49:17 +0900

    📑 :: GetCompanyDetailUseCase

---
+++ yapaghetti-application/src/main/java/kr/hs/entrydsm/yapaghetti/domain/teacher/usecase/GetCompanyDetailUseCase.java
@@ -0,0 +1,36 @@
+package kr.hs.entrydsm.yapaghetti.domain.teacher.usecase;
+
+import kr.hs.entrydsm.yapaghetti.annotation.UseCase;
+import kr.hs.entrydsm.yapaghetti.domain.company.domain.Company;
+import kr.hs.entrydsm.yapaghetti.domain.teacher.api.GetCompanyDetailPort;
+import kr.hs.entrydsm.yapaghetti.domain.teacher.api.dto.response.CompanyDetailResponse;
+import kr.hs.entrydsm.yapaghetti.domain.teacher.spi.TeacherQueryCompanyPort;
+import kr.hs.entrydsm.yapaghetti.domain.teacher.spi.TeacherQueryUserPort;
+import kr.hs.entrydsm.yapaghetti.domain.user.domain.User;
+import lombok.RequiredArgsConstructor;
+
+import java.util.UUID;
+
+@RequiredArgsConstructor
+@UseCase
+public class GetCompanyDetailUseCase implements GetCompanyDetailPort {
+
+    private final TeacherQueryCompanyPort teacherQueryCompanyPort;
+    private final TeacherQueryUserPort teacherQueryUserPort;
+
+    public CompanyDetailResponse execute(UUID companyId) {
+        Company company = teacherQueryCompanyPort.queryCompanyByUserId(companyId);
+        User user = teacherQueryUserPort.queryUserById(company.getUserId());
+
+        return CompanyDetailResponse.builder()
+                .companyId(companyId.toString())
+                .name(user.getName())
+                .companyName(company.getCompanyName())
+                .email(user.getEmail())
+                .phoneNumber(user.getPhoneNumber())
+                .location(user.getLocation())
+                .profileImagePath(user.getProfileImagePath())
+                .build();
+    }
+
+}
