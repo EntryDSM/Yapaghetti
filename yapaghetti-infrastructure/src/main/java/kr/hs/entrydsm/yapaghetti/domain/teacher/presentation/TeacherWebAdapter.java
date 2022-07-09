259f4ac5c929115e99ee8961f586d5a5ea20fb29
osangu <hhgnoon@naver.com>
2022-07-09 23:36:41 +0900

    ⚡️ :: UpdateCompany API

--- yapaghetti-infrastructure/src/main/java/kr/hs/entrydsm/yapaghetti/domain/teacher/presentation/TeacherWebAdapter.java
+++ yapaghetti-infrastructure/src/main/java/kr/hs/entrydsm/yapaghetti/domain/teacher/presentation/TeacherWebAdapter.java
@@ -3,11 +3,15 @@ package kr.hs.entrydsm.yapaghetti.domain.teacher.presentation;
 import kr.hs.entrydsm.yapaghetti.domain.teacher.api.CreateFeedbackPort;
 import kr.hs.entrydsm.yapaghetti.domain.teacher.api.DeleteCompanyPort;
 import kr.hs.entrydsm.yapaghetti.domain.teacher.api.DeleteStudentPort;
+import kr.hs.entrydsm.yapaghetti.domain.teacher.api.UpdateCompanyPort;
 import kr.hs.entrydsm.yapaghetti.domain.teacher.api.dto.request.DomainCreateFeedbackRequest;
+import kr.hs.entrydsm.yapaghetti.domain.teacher.api.dto.request.DomainUpdateCompanyRequest;
 import kr.hs.entrydsm.yapaghetti.domain.teacher.presentation.dto.request.WebCreateFeedbackRequest;
+import kr.hs.entrydsm.yapaghetti.domain.teacher.presentation.dto.request.WebUpdateCompanyRequest;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -26,6 +30,7 @@ public class TeacherWebAdapter {
 
     private final CreateFeedbackPort createFeedbackPort;
     private final DeleteStudentPort deleteStudentPort;
+    private final UpdateCompanyPort updateCompanyPort;
     private final DeleteCompanyPort deleteCompanyPort;
 
     @ResponseStatus(HttpStatus.CREATED)
@@ -45,6 +50,24 @@ public class TeacherWebAdapter {
     @DeleteMapping("/student/{student-id}")
     public void deleteStudent(@PathVariable("student-id") @NotBlank UUID studentId) {
         deleteStudentPort.execute(studentId);
+    }
+
+    @ResponseStatus(HttpStatus.OK)
+    @PatchMapping("/company/{company-id}")
+    public void updateCompany(@PathVariable("company-id") @NotBlank UUID companyId,
+                              @RequestBody @Valid WebUpdateCompanyRequest request) {
+        updateCompanyPort.execute(
+                DomainUpdateCompanyRequest.builder()
+                        .companyId(companyId)
+                        .companyName(request.getCompanyName())
+                        .name(request.getName())
+                        .email(request.getEmail())
+                        .phoneNumber(request.getPhoneNumber())
+                        .location(request.getLocation())
+                        .startAt(request.getStartAt())
+                        .endAt(request.getEndAt())
+                    .build()
+        );
     }
 
     @ResponseStatus(HttpStatus.NO_CONTENT)
