607b47c488b12224163478c1e6cbdea5a05cf916
이준서 <ljchan71@gmail.com>
2022-07-09 19:20:43 +0900

    📑 :: AuthCodeMapper

---
+++ yapaghetti-infrastructure/src/main/java/kr/hs/entrydsm/yapaghetti/domain/auth/mapper/AuthCodeMapper.java
@@ -0,0 +1,31 @@
+package kr.hs.entrydsm.yapaghetti.domain.auth.mapper;
+
+import kr.hs.entrydsm.yapaghetti.domain.auth.domain.AuthCode;
+import kr.hs.entrydsm.yapaghetti.domain.auth.persistence.entity.AuthCodeEntity;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+
+@RequiredArgsConstructor
+@Component
+public class AuthCodeMapper {
+
+	public AuthCodeEntity domainToEntity(AuthCode authCode) {
+		return AuthCodeEntity.builder()
+			.value(authCode.getValue())
+			.authCode(authCode.getAuthCode())
+			.type(authCode.getType())
+			.isVerify(authCode.isVerify())
+			.timeToLive(authCode.getTimeToLive())
+			.build();
+	}
+
+	public AuthCode entityToDomain(AuthCodeEntity authCodeEntity) {
+		return AuthCode.builder()
+			.value(authCodeEntity.getValue())
+			.authCode(authCodeEntity.getAuthCode())
+			.type(authCodeEntity.getType())
+			.timeToLive(authCodeEntity.getTimeToLive())
+			.isVerify(authCodeEntity.isVerify())
+			.build();
+	}
+}
