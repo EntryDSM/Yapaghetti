bc51f0a16d542039a91f705dd5053465215e6a50
KimBeomJin <sdpthf@naver.com>
2022-07-09 18:14:32 +0900

    📑 :: ApproveStayDocumentTest

---
+++ yapaghetti-application/src/test/java/kr/hs/entrydsm/yapaghetti/domain/document/usecase/ApproveStayDocumentTest.java
@@ -0,0 +1,46 @@
+package kr.hs.entrydsm.yapaghetti.domain.document.usecase;
+
+import kr.hs.entrydsm.yapaghetti.domain.document.domain.Document;
+import kr.hs.entrydsm.yapaghetti.domain.document.domain.DocumentType;
+import kr.hs.entrydsm.yapaghetti.domain.document.spi.CommandDocumentPort;
+import kr.hs.entrydsm.yapaghetti.domain.document.spi.QueryDocumentPort;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.UUID;
+
+import static org.mockito.BDDMockito.given;
+
+@ExtendWith(MockitoExtension.class)
+class ApproveStayDocumentTest {
+
+    @Mock
+    QueryDocumentPort queryDocumentPort;
+
+    @Mock
+    CommandDocumentPort commandDocumentPort;
+
+    @InjectMocks
+    ApproveStayDocumentUseCase approveStayDocumentUseCase;
+
+    @Test
+    void approveDocument() {
+        UUID documentId = UUID.randomUUID();
+        UUID userId = UUID.randomUUID();
+
+        given(queryDocumentPort.queryDocumentByIdAndType(documentId, DocumentType.STAY)).willReturn(
+                Document.builder()
+                        .id(documentId)
+                        .previewImagePath("testPreviewImagePath")
+                        .content("testContent")
+                        .type(DocumentType.STAY)
+                        .userId(userId)
+                        .build()
+        );
+
+        approveStayDocumentUseCase.execute(documentId);
+    }
+}
