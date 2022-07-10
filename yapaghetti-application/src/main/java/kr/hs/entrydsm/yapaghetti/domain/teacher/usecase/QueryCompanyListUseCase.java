b520914417fc3208c338bde1c1c4c5ead6e7fc2d
osangu <hhgnoon@naver.com>
2022-07-10 18:14:36 +0900

    📑 :: QueryCompanyListUseCase

---
+++ yapaghetti-application/src/main/java/kr/hs/entrydsm/yapaghetti/domain/teacher/usecase/QueryCompanyListUseCase.java
@@ -0,0 +1,23 @@
+package kr.hs.entrydsm.yapaghetti.domain.teacher.usecase;
+
+import kr.hs.entrydsm.yapaghetti.annotation.UseCase;
+import kr.hs.entrydsm.yapaghetti.domain.teacher.api.QueryCompanyListPort;
+import kr.hs.entrydsm.yapaghetti.domain.teacher.api.dto.response.CompanyListResponse;
+import kr.hs.entrydsm.yapaghetti.domain.teacher.spi.TeacherQueryCompanyPort;
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
+@UseCase
+public class QueryCompanyListUseCase implements QueryCompanyListPort {
+
+    private final TeacherQueryCompanyPort teacherQueryCompanyPort;
+
+    @Override
+    public CompanyListResponse execute(String name) {
+        return new CompanyListResponse(
+                teacherQueryCompanyPort.queryCompanyDetailList(name)
+        );
+    }
+
+
+}
