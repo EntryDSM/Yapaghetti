5c7c94ab6a259d37c1f75ecbdcb72cf12ccf78e1
이준서 <ljchan71@gmail.com>
2022-07-10 23:54:54 +0900

    ⚡️ :: AuthCode

--- yapaghetti-application/src/main/java/kr/hs/entrydsm/yapaghetti/domain/auth/domain/AuthCode.java
+++ yapaghetti-application/src/main/java/kr/hs/entrydsm/yapaghetti/domain/auth/domain/AuthCode.java
@@ -1,5 +1,6 @@
 package kr.hs.entrydsm.yapaghetti.domain.auth.domain;
 
+import java.time.LocalDateTime;
 import kr.hs.entrydsm.yapaghetti.annotation.Aggregate;
 import lombok.Builder;
 import lombok.Getter;
@@ -17,6 +18,19 @@ public class AuthCode {
 
 	private final Long timeToLive;
 
+	private final LocalDateTime authTime;
+
 	private final boolean isVerify;
+
+	public AuthCode refresh(String authCode, Long authTime) {
+		return AuthCode.builder()
+			.value(this.getValue())
+			.authCode(authCode)
+			.type(this.getType())
+			.timeToLive(timeToLive + authTime)
+			.authTime(this.authTime.plusSeconds(authTime))
+			.isVerify(this.isVerify)
+			.build();
+	}
 
 }
