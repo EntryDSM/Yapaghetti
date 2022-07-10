4b8edd54e91c9b155f0ebdcce029f3c9d7a15094
이준서 <ljchan71@gmail.com>
2022-07-10 23:57:23 +0900

    ♻️ :: SendEmailAuthCodeUseCase

--- yapaghetti-application/src/main/java/kr/hs/entrydsm/yapaghetti/domain/auth/usecase/SendEmailAuthCodeUseCase.java
+++ yapaghetti-application/src/main/java/kr/hs/entrydsm/yapaghetti/domain/auth/usecase/SendEmailAuthCodeUseCase.java
@@ -1,17 +1,17 @@
 package kr.hs.entrydsm.yapaghetti.domain.auth.usecase;
 
+import java.time.LocalDateTime;
 import kr.hs.entrydsm.yapaghetti.annotation.UseCase;
 import kr.hs.entrydsm.yapaghetti.domain.auth.api.SendEmailAuthCodePort;
 import kr.hs.entrydsm.yapaghetti.domain.auth.domain.AuthCode;
-import kr.hs.entrydsm.yapaghetti.domain.auth.domain.AuthCodeLimit;
 import kr.hs.entrydsm.yapaghetti.domain.auth.domain.AuthCodeType;
 import kr.hs.entrydsm.yapaghetti.domain.auth.exception.AuthCodeOverLimitException;
+import kr.hs.entrydsm.yapaghetti.domain.auth.spi.GetAuthPropertiesPort;
 import kr.hs.entrydsm.yapaghetti.domain.auth.spi.AuthQueryUserPort;
 import kr.hs.entrydsm.yapaghetti.domain.auth.spi.AuthSecurityPort;
-import kr.hs.entrydsm.yapaghetti.domain.auth.spi.CommandAuthCodeLimitPort;
 import kr.hs.entrydsm.yapaghetti.domain.auth.spi.CommandAuthCodePort;
 import kr.hs.entrydsm.yapaghetti.domain.auth.spi.GenerateRandomStringPort;
-import kr.hs.entrydsm.yapaghetti.domain.auth.spi.QueryAuthCodeLimitPort;
+import kr.hs.entrydsm.yapaghetti.domain.auth.spi.QueryAuthCodePort;
 import kr.hs.entrydsm.yapaghetti.domain.auth.spi.SendMailPort;
 import kr.hs.entrydsm.yapaghetti.domain.user.domain.User;
 import lombok.RequiredArgsConstructor;
@@ -24,49 +24,42 @@ public class SendEmailAuthCodeUseCase implements SendEmailAuthCodePort {
 	private final AuthQueryUserPort authQueryUserPort;
 	private final GenerateRandomStringPort generateRandomStringPort;
 	private final SendMailPort sendMailPort;
+	private final QueryAuthCodePort queryAuthCodePort;
 	private final CommandAuthCodePort commandAuthCodePort;
-	private final QueryAuthCodeLimitPort queryAuthCodeLimitPort;
-	private final CommandAuthCodeLimitPort commandAuthCodeLimitPort;
+	private final GetAuthPropertiesPort getAuthPropertiesPort;
 
 	@Override
 	public void execute() {
 		AuthCodeType type = AuthCodeType.EMAIL;
-		Long ttl = 300L;
+		Long authTime = getAuthPropertiesPort.getAuthTime();
+		String authCode = generateRandomStringPort.getAuthCode();
+		AuthCode emailAuthCode;
 
 		User user = authQueryUserPort.queryUserById(
 			authSecurityPort.getCurrentUserId()
 		);
-
 		String value = user.getEmail();
-		String authCode = generateRandomStringPort.getAuthCode();
-		AuthCodeLimit authCodeLimit;
 
 		if (commandAuthCodePort.existsAuthCodeById(value)) {
-			authCodeLimit = queryAuthCodeLimitPort.queryAuthCodeLimitById(value)
-				.upCount();
+			emailAuthCode = queryAuthCodePort.queryAuthCodeById(value);
 
-			if (authCodeLimit.getCount() > 5) {
+			if (emailAuthCode.getTimeToLive() > getAuthPropertiesPort.getLimitTime()) {
 				throw AuthCodeOverLimitException.EXCEPTION;
 			}
+
+			emailAuthCode = emailAuthCode.refresh(authCode, authTime);
 		} else {
-			authCodeLimit = AuthCodeLimit.builder()
+			emailAuthCode = AuthCode.builder()
 				.value(value)
+				.authCode(authCode)
 				.type(type)
-				.timeToLive(ttl * 10)
-				.count(1)
+				.isVerify(false)
+				.timeToLive(authTime)
+				.authTime(LocalDateTime.now().plusSeconds(authTime))
 				.build();
 		}
 
-		AuthCode emailAuthCode = AuthCode.builder()
-			.value(value)
-			.authCode(authCode)
-			.type(type)
-			.isVerify(false)
-			.timeToLive(ttl)
-			.build();
-
 		commandAuthCodePort.saveAuthCode(emailAuthCode);
-		commandAuthCodeLimitPort.saveAuthCodeLimit(authCodeLimit);
 
 		sendMailPort.sendAuthCode(value, authCode);
 	}
