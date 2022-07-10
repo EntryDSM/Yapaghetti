76be2eb8e77cd59cc89a81f809cc85f06a88f2ea
이준서 <ljchan71@gmail.com>
2022-07-10 23:56:58 +0900

    ⚡️ :: chore

--- yapaghetti-infrastructure/src/main/java/kr/hs/entrydsm/yapaghetti/global/util/GenerateRandomStringUtilsAdapter.java
+++ yapaghetti-infrastructure/src/main/java/kr/hs/entrydsm/yapaghetti/global/util/GenerateRandomStringUtilsAdapter.java
@@ -2,7 +2,6 @@ package kr.hs.entrydsm.yapaghetti.global.util;
 
 import kr.hs.entrydsm.yapaghetti.domain.auth.spi.GenerateRandomStringPort;
 import kr.hs.entrydsm.yapaghetti.global.annotation.Adapter;
-import lombok.RequiredArgsConstructor;
 import org.apache.commons.lang3.RandomStringUtils;
 
 @Adapter
