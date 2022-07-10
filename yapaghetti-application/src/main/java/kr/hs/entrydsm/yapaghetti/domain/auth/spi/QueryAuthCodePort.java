6a36c7e7dd45bdee909ef6e53a137ac794091433
이준서 <ljchan71@gmail.com>
2022-07-10 23:56:10 +0900

    📑 :: QueryAuthCodePort

---
+++ yapaghetti-application/src/main/java/kr/hs/entrydsm/yapaghetti/domain/auth/spi/QueryAuthCodePort.java
@@ -0,0 +1,7 @@
+package kr.hs.entrydsm.yapaghetti.domain.auth.spi;
+
+import kr.hs.entrydsm.yapaghetti.domain.auth.domain.AuthCode;
+
+public interface QueryAuthCodePort {
+	AuthCode queryAuthCodeById(String id);
+}
