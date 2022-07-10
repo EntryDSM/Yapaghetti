3b3124bdf1f58316d3ef00e246ac947d772fef75
osangu <hhgnoon@naver.com>
2022-07-10 23:39:17 +0900

    ⚡️ ::  queryStudentDetailById

--- yapaghetti-application/src/main/java/kr/hs/entrydsm/yapaghetti/domain/teacher/spi/TeacherQueryStudentPort.java
+++ yapaghetti-application/src/main/java/kr/hs/entrydsm/yapaghetti/domain/teacher/spi/TeacherQueryStudentPort.java
@@ -1,9 +1,11 @@
 package kr.hs.entrydsm.yapaghetti.domain.teacher.spi;
 
 import kr.hs.entrydsm.yapaghetti.domain.student.domain.Student;
+import kr.hs.entrydsm.yapaghetti.domain.teacher.api.dto.response.StudentDetailInformation;
 
 import java.util.UUID;
 
 public interface TeacherQueryStudentPort {
     Student queryUserById(UUID studentId);
+    StudentDetailInformation queryStudentDetail(UUID studentId);
 }
