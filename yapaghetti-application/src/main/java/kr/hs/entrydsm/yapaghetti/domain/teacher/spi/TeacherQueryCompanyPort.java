d0b63e41ab7d31fc7f98f499cd8b8477303144c6
osangu <hhgnoon@naver.com>
2022-07-10 18:14:10 +0900

    ⚡️ :: queryCompanyDetailList

--- yapaghetti-application/src/main/java/kr/hs/entrydsm/yapaghetti/domain/teacher/spi/TeacherQueryCompanyPort.java
+++ yapaghetti-application/src/main/java/kr/hs/entrydsm/yapaghetti/domain/teacher/spi/TeacherQueryCompanyPort.java
@@ -1,9 +1,12 @@
 package kr.hs.entrydsm.yapaghetti.domain.teacher.spi;
 
 import kr.hs.entrydsm.yapaghetti.domain.company.domain.Company;
+import kr.hs.entrydsm.yapaghetti.domain.teacher.api.dto.response.CompanyElement;
 
+import java.util.List;
 import java.util.UUID;
 
 public interface TeacherQueryCompanyPort {
     Company queryCompanyByUserId(UUID companyId);
+    List<CompanyElement> queryCompanyDetailList(String name);
 }
--- yapaghetti-infrastructure/src/main/java/kr/hs/entrydsm/yapaghetti/domain/company/persistence/CompanyPersistenceAdapter.java
+++ yapaghetti-infrastructure/src/main/java/kr/hs/entrydsm/yapaghetti/domain/company/persistence/CompanyPersistenceAdapter.java
@@ -1,15 +1,20 @@
 package kr.hs.entrydsm.yapaghetti.domain.company.persistence;
 
+import com.querydsl.jpa.impl.JPAQueryFactory;
 import kr.hs.entrydsm.yapaghetti.domain.company.domain.Company;
 import kr.hs.entrydsm.yapaghetti.domain.company.exception.CompanyNotFoundException;
 import kr.hs.entrydsm.yapaghetti.domain.company.mapper.CompanyMapper;
-import kr.hs.entrydsm.yapaghetti.domain.company.spi.CommandCompanyPort;
 import kr.hs.entrydsm.yapaghetti.domain.company.spi.CompanyPort;
-import kr.hs.entrydsm.yapaghetti.domain.company.spi.QueryCompanyPort;
+import kr.hs.entrydsm.yapaghetti.domain.teacher.api.dto.response.CompanyElement;
 import kr.hs.entrydsm.yapaghetti.global.annotation.Adapter;
 import lombok.RequiredArgsConstructor;
 
+import java.util.List;
 import java.util.UUID;
+
+import static com.querydsl.core.types.Projections.constructor;
+import static kr.hs.entrydsm.yapaghetti.domain.company.persistence.entity.QCompanyEntity.companyEntity;
+import static kr.hs.entrydsm.yapaghetti.domain.user.persistence.entity.QUserEntity.userEntity;
 
 @RequiredArgsConstructor
 @Adapter
@@ -18,12 +23,32 @@ public class CompanyPersistenceAdapter implements CompanyPort {
     private final CompanyRepository companyRepository;
     private final CompanyMapper companyMapper;
 
+    private final JPAQueryFactory jpaQueryFactory;
+
     @Override
     public Company queryCompanyByUserId(UUID userId) {
         return companyMapper.entityToDomain(
                 companyRepository.findByUserEntityId(userId)
                         .orElseThrow(() -> CompanyNotFoundException.EXCEPTION)
         );
+    }
+
+    @Override
+    public List<CompanyElement> queryCompanyDetailList(String name) {
+        return jpaQueryFactory
+                .select(
+                        constructor(
+                                CompanyElement.class,
+                                companyEntity.userId,
+                                userEntity.profileImagePath,
+                                companyEntity.companyName,
+                                userEntity.email
+                        )
+                )
+                .from(companyEntity)
+                .where(companyEntity.companyName.like("%"+name+"%"))
+                .join(companyEntity.userEntity, userEntity)
+                .fetch();
     }
 
     @Override
