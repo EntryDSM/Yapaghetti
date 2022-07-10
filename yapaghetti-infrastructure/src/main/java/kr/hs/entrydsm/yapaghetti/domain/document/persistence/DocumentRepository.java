377af21e9bae03de231f0417117ed05fa6b3063f
정대현 <eogus_2513@dsm.hs.kr>
2022-07-10 23:41:26 +0900

    ⚡️ :: findAllByUserEntityIdAndTypeNotIn 추가

--- yapaghetti-infrastructure/src/main/java/kr/hs/entrydsm/yapaghetti/domain/document/persistence/DocumentRepository.java
+++ yapaghetti-infrastructure/src/main/java/kr/hs/entrydsm/yapaghetti/domain/document/persistence/DocumentRepository.java
@@ -2,13 +2,13 @@ package kr.hs.entrydsm.yapaghetti.domain.document.persistence;
 
 import kr.hs.entrydsm.yapaghetti.domain.document.domain.DocumentType;
 import kr.hs.entrydsm.yapaghetti.domain.document.persistence.entity.DocumentEntity;
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
-public interface DocumentRepository extends CrudRepository<DocumentEntity, UUID> {
+public interface DocumentRepository extends JpaRepository<DocumentEntity, UUID> {
     Optional<DocumentEntity> findByUserEntityIdAndType(UUID userId, DocumentType type);
 
     Optional<DocumentEntity> findByIdAndType(UUID documentId, DocumentType type);
@@ -16,4 +16,6 @@ public interface DocumentRepository extends CrudRepository<DocumentEntity, UUID>
     Optional<DocumentEntity> findByIdAndUserEntityIdAndType(UUID id, UUID userId, DocumentType type);
 
     List<DocumentEntity> findAllByUserEntityIdAndType(UUID userId, DocumentType type);
+
+    List<DocumentEntity> findAllByUserEntityIdAndTypeNotIn(UUID userId, List<DocumentType> type);
 }
