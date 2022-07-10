27a60bd07d01eba53d030841a21a2ff5936553da
정대현 <eogus_2513@dsm.hs.kr>
2022-07-10 23:43:48 +0900

    :recycle: :: 바뀐 명세에 따라 수정

--- yapaghetti-application/src/main/java/kr/hs/entrydsm/yapaghetti/domain/document/api/dto/response/QueryStayAndPublicDocumentPreviewResponse.java
+++ yapaghetti-application/src/main/java/kr/hs/entrydsm/yapaghetti/domain/document/api/dto/response/QueryStayAndPublicDocumentPreviewResponse.java
@@ -3,10 +3,11 @@ package kr.hs.entrydsm.yapaghetti.domain.document.api.dto.response;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
+import java.util.List;
+
 @Getter
 @AllArgsConstructor
 public class QueryStayAndPublicDocumentPreviewResponse {
 
-    private final String stayPreviewImagePath;
-    private final String publicPreviewImagePath;
+    private final List<StayAndPublicDocumentElement> documentList;
 }
