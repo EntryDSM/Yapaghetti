18d0443c2f8039358b6aa18672c65188fcadb462
이준서 <ljchan71@gmail.com>
2022-07-11 12:37:41 +0900

    📑 :: AuthCodeAlreadyTimeOutException

---
+++ yapaghetti-application/src/main/java/kr/hs/entrydsm/yapaghetti/domain/auth/exception/AuthCodeAlreadyTimeOutException.java
@@ -0,0 +1,13 @@
+package kr.hs.entrydsm.yapaghetti.domain.auth.exception;
+
+import kr.hs.entrydsm.yapaghetti.domain.auth.error.AuthCodeErrorCode;
+import kr.hs.entrydsm.yapaghetti.error.YapaghettiException;
+
+public class AuthCodeAlreadyTimeOutException extends YapaghettiException {
+
+	private AuthCodeAlreadyTimeOutException() {
+		super(AuthCodeErrorCode.AUTH_CODE_ALREADY_TIMEOUT);
+	}
+
+	public static final YapaghettiException EXCEPTION = new AuthCodeAlreadyTimeOutException();
+}
