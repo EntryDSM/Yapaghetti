aa98f7120afbf87e7124ceab605bfe668bfef529
osangu <hhgnoon@naver.com>
2022-07-11 16:10:54 +0900

    ♻️ :: 불필요한 객체 삭제

--- yapaghetti-application/src/main/java/kr/hs/entrydsm/yapaghetti/domain/teacher/usecase/UpdateCompanyUseCase.java
+++ yapaghetti-application/src/main/java/kr/hs/entrydsm/yapaghetti/domain/teacher/usecase/UpdateCompanyUseCase.java
@@ -5,17 +5,12 @@ import kr.hs.entrydsm.yapaghetti.domain.company.domain.Company;
 import kr.hs.entrydsm.yapaghetti.domain.teacher.api.UpdateCompanyPort;
 import kr.hs.entrydsm.yapaghetti.domain.teacher.api.dto.request.DomainUpdateCompanyRequest;
 import kr.hs.entrydsm.yapaghetti.domain.teacher.spi.TeacherCommandCompanyPort;
-import kr.hs.entrydsm.yapaghetti.domain.teacher.spi.TeacherCommandUserPort;
 import kr.hs.entrydsm.yapaghetti.domain.teacher.spi.TeacherQueryCompanyPort;
-import kr.hs.entrydsm.yapaghetti.domain.teacher.spi.TeacherQueryUserPort;
 import lombok.RequiredArgsConstructor;
 
 @RequiredArgsConstructor
 @UseCase
 public class UpdateCompanyUseCase implements UpdateCompanyPort {
-
-    private final TeacherCommandUserPort teacherCommandUserPort;
-    private final TeacherQueryUserPort teacherQueryUserPort;
     private final TeacherCommandCompanyPort teacherCommandCompanyPort;
     private final TeacherQueryCompanyPort teacherQueryCompanyPort;
 
