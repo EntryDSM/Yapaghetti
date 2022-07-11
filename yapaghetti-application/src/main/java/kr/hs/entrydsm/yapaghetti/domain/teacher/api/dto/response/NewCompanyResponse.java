214ef66e678e1f4982c00b4dc33eeaa5821548ff
osangu <hhgnoon@naver.com>
2022-07-11 21:17:23 +0900

    📑 :: NewCompanyResponse

---
+++ yapaghetti-application/src/main/java/kr/hs/entrydsm/yapaghetti/domain/teacher/api/dto/response/NewCompanyResponse.java
@@ -0,0 +1,8 @@
+package kr.hs.entrydsm.yapaghetti.domain.teacher.api.dto.response;
+
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
+public class NewCompanyResponse {
+    private final String password;
+}
