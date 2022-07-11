acb713618702cd18f019efe9e59a90a79ffff174
정대현 <eogus_2513@dsm.hs.kr>
2022-07-11 09:07:03 +0900

    :coffin: :: @Builder 어노테이션 삭제

--- yapaghetti-application/src/main/java/kr/hs/entrydsm/yapaghetti/domain/document/api/dto/response/StayAndPublicDocumentElement.java
+++ yapaghetti-application/src/main/java/kr/hs/entrydsm/yapaghetti/domain/document/api/dto/response/StayAndPublicDocumentElement.java
@@ -2,11 +2,9 @@ package kr.hs.entrydsm.yapaghetti.domain.document.api.dto.response;
 
 import kr.hs.entrydsm.yapaghetti.domain.document.domain.DocumentType;
 import lombok.AllArgsConstructor;
-import lombok.Builder;
 import lombok.Getter;
 
 @Getter
-@Builder
 @AllArgsConstructor
 public class StayAndPublicDocumentElement {
 
