2d2d7c5d8f427b7c5524765328bbbadf730c3cff
이준서 <ljchan71@gmail.com>
2022-07-11 12:39:02 +0900

    ⚡️ :: AuthWebAdapter

--- yapaghetti-infrastructure/src/main/java/kr/hs/entrydsm/yapaghetti/domain/auth/presentation/AuthWebAdapter.java
+++ yapaghetti-infrastructure/src/main/java/kr/hs/entrydsm/yapaghetti/domain/auth/presentation/AuthWebAdapter.java
@@ -1,9 +1,12 @@
 package kr.hs.entrydsm.yapaghetti.domain.auth.presentation;
 
+import javax.validation.constraints.NotBlank;
 import kr.hs.entrydsm.yapaghetti.domain.auth.api.SendEmailAuthCodePort;
+import kr.hs.entrydsm.yapaghetti.domain.auth.api.VerifyEmailAuthCodePort;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RequiredArgsConstructor
@@ -12,9 +15,15 @@ import org.springframework.web.bind.annotation.RestController;
 public class AuthWebAdapter {
 
 	private final SendEmailAuthCodePort sendEmailAuthCodePort;
+	private final VerifyEmailAuthCodePort verifyEmailAuthCodePort;
 
 	@PostMapping("/email")
 	public void sendEmailAuthCode() {
 		sendEmailAuthCodePort.execute();
+	}
+
+	@RequestMapping(value = "/email", method = RequestMethod.HEAD)
+	public void verifyEmailAuthCode(@NotBlank String authCode) {
+		verifyEmailAuthCodePort.execute(authCode);
 	}
 }
