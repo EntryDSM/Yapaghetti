c1e2a1761e118005aae59d291816ce4ce80d06c8
정대현 <eogus_2513@dsm.hs.kr>
2022-07-11 14:40:02 +0900

    :recycle: :: student major tag 가져오는 로직 추가

--- yapaghetti-application/src/main/java/kr/hs/entrydsm/yapaghetti/domain/document/usecase/QueryLocalDocumentListUseCase.java
+++ yapaghetti-application/src/main/java/kr/hs/entrydsm/yapaghetti/domain/document/usecase/QueryLocalDocumentListUseCase.java
@@ -5,10 +5,12 @@ import kr.hs.entrydsm.yapaghetti.domain.document.api.QueryLocalDocumentListPort;
 import kr.hs.entrydsm.yapaghetti.domain.document.api.dto.response.DocumentElement;
 import kr.hs.entrydsm.yapaghetti.domain.document.api.dto.response.QueryLocalDocumentListResponse;
 import kr.hs.entrydsm.yapaghetti.domain.document.domain.DocumentType;
+import kr.hs.entrydsm.yapaghetti.domain.document.spi.DocumentQueryStudentPort;
 import kr.hs.entrydsm.yapaghetti.domain.document.spi.DocumentQueryTagPort;
 import kr.hs.entrydsm.yapaghetti.domain.document.spi.DocumentQueryUserPort;
 import kr.hs.entrydsm.yapaghetti.domain.document.spi.DocumentSecurityPort;
 import kr.hs.entrydsm.yapaghetti.domain.document.spi.QueryDocumentPort;
+import kr.hs.entrydsm.yapaghetti.domain.student.domain.Student;
 import kr.hs.entrydsm.yapaghetti.domain.tag.domain.Tag;
 import kr.hs.entrydsm.yapaghetti.domain.user.domain.User;
 import lombok.RequiredArgsConstructor;
@@ -23,6 +25,7 @@ public class QueryLocalDocumentListUseCase implements QueryLocalDocumentListPort
 
     private final DocumentQueryUserPort documentQueryUserPort;
     private final DocumentSecurityPort documentSecurityPort;
+    private final DocumentQueryStudentPort documentQueryStudentPort;
     private final DocumentQueryTagPort documentQueryTagPort;
     private final QueryDocumentPort queryDocumentPort;
 
@@ -30,8 +33,9 @@ public class QueryLocalDocumentListUseCase implements QueryLocalDocumentListPort
     public QueryLocalDocumentListResponse execute() {
         UUID currentUserId = documentSecurityPort.getCurrentUserId();
         User user = documentQueryUserPort.queryUserById(currentUserId);
+        Student student = documentQueryStudentPort.queryStudentById(currentUserId);
 
-        Tag majorTag = documentQueryTagPort.queryMajorTagByStudentId(currentUserId);
+        Tag majorTag = documentQueryTagPort.queryTagById(student.getTagId());
 
         List<DocumentElement> localDocumentList =
                 queryDocumentPort.queryDocumentAllByUserIdAndType(currentUserId, DocumentType.LOCAL)
