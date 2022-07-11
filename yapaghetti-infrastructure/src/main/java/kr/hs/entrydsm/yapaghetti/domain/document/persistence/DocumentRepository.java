191453196b1aedcaea8e367506b06200c1368ceb
정대현 <eogus_2513@dsm.hs.kr>
2022-07-11 10:26:02 +0900

    :recycle: :: JpaRepositoru -> CrudRepository로 수정

--- yapaghetti-infrastructure/src/main/java/kr/hs/entrydsm/yapaghetti/domain/document/persistence/DocumentRepository.java
+++ yapaghetti-infrastructure/src/main/java/kr/hs/entrydsm/yapaghetti/domain/document/persistence/DocumentRepository.java
@@ -2,13 +2,13 @@ package kr.hs.entrydsm.yapaghetti.domain.document.persistence;
 
 import kr.hs.entrydsm.yapaghetti.domain.document.domain.DocumentType;
 import kr.hs.entrydsm.yapaghetti.domain.document.persistence.entity.DocumentEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.CrudRepository;
 
 import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
-public interface DocumentRepository extends JpaRepository<DocumentEntity, UUID> {
+public interface DocumentRepository extends CrudRepository<DocumentEntity, UUID> {
     Optional<DocumentEntity> findByUserEntityIdAndType(UUID userId, DocumentType type);
 
     Optional<DocumentEntity> findByIdAndType(UUID documentId, DocumentType type);
