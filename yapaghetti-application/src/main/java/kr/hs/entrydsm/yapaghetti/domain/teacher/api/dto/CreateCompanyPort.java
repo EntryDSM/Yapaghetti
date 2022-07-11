54da57bdf1bea3112895b0388a0d32d05bc60336
osangu <hhgnoon@naver.com>
2022-07-11 21:17:31 +0900

    📑 :: CreateCompanyPort

---
+++ yapaghetti-application/src/main/java/kr/hs/entrydsm/yapaghetti/domain/teacher/api/dto/CreateCompanyPort.java
@@ -0,0 +1,7 @@
+package kr.hs.entrydsm.yapaghetti.domain.teacher.api.dto;
+
+import kr.hs.entrydsm.yapaghetti.domain.teacher.api.dto.response.NewCompanyResponse;
+
+public interface CreateCompanyPort {
+    NewCompanyResponse execute();
+}
