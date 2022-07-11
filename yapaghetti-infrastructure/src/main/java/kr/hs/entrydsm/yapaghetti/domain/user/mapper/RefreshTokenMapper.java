d0eb238b2fbb9f721ea9fd651870c34482623053
정대현 <eogus_2513@dsm.hs.kr>
2022-07-11 15:15:45 +0900

    :recycle: :: @Mapper(componentModel = "spring") 추가

--- yapaghetti-infrastructure/src/main/java/kr/hs/entrydsm/yapaghetti/domain/user/mapper/RefreshTokenMapper.java
+++ yapaghetti-infrastructure/src/main/java/kr/hs/entrydsm/yapaghetti/domain/user/mapper/RefreshTokenMapper.java
@@ -4,7 +4,7 @@ import kr.hs.entrydsm.yapaghetti.domain.user.domain.RefreshToken;
 import kr.hs.entrydsm.yapaghetti.domain.user.persistence.entity.RefreshTokenEntity;
 import org.mapstruct.Mapper;
 
-@Mapper
+@Mapper(componentModel = "spring")
 public interface RefreshTokenMapper {
     RefreshToken entityToDomain(RefreshTokenEntity refreshTokenEntity);
     RefreshTokenEntity domainToEntity(RefreshToken refreshToken);
