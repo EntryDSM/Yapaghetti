6a71a26be65e55daa0d30eb7b3b685af76d42164
이준서 <ljchan71@gmail.com>
2022-07-12 11:31:39 +0900

    ⚡️ :: AuthWebAdapter

--- yapaghetti-infrastructure/src/main/java/kr/hs/entrydsm/yapaghetti/domain/auth/presentation/AuthWebAdapter.java
+++ yapaghetti-infrastructure/src/main/java/kr/hs/entrydsm/yapaghetti/domain/auth/presentation/AuthWebAdapter.java
@@ -2,9 +2,11 @@ package kr.hs.entrydsm.yapaghetti.domain.auth.presentation;
 
 import javax.validation.constraints.NotBlank;
 import kr.hs.entrydsm.yapaghetti.domain.auth.api.SendEmailAuthCodePort;
+import kr.hs.entrydsm.yapaghetti.domain.auth.api.SendPhoneNumberAuthCodePort;
 import kr.hs.entrydsm.yapaghetti.domain.auth.api.VerifyEmailAuthCodePort;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -16,11 +18,17 @@ import org.springframework.web.bind.annotation.RestController;
 public class AuthWebAdapter {
 
 	private final SendEmailAuthCodePort sendEmailAuthCodePort;
+	private final SendPhoneNumberAuthCodePort sendPhoneNumberAuthCodePort;
 	private final VerifyEmailAuthCodePort verifyEmailAuthCodePort;
 
 	@PostMapping("/email")
 	public void sendEmailAuthCode() {
 		sendEmailAuthCodePort.execute();
+	}
+
+	@PostMapping("/phone-number")
+	public void sendPhoneNumberAuthCode(@RequestBody @NotBlank String phoneNumber) {
+		sendPhoneNumberAuthCodePort.execute(phoneNumber);
 	}
 
 	@RequestMapping(value = "/email", method = RequestMethod.HEAD)
