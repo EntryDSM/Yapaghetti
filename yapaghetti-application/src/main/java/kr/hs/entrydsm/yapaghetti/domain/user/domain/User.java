796d1b705845fbb9969607444af0c3fe669d4e6e
osangu <hhgnoon@naver.com>
2022-07-12 18:04:15 +0900

    ♻️ :: User 함수 변경

--- yapaghetti-application/src/main/java/kr/hs/entrydsm/yapaghetti/domain/user/domain/User.java
+++ yapaghetti-application/src/main/java/kr/hs/entrydsm/yapaghetti/domain/user/domain/User.java
@@ -57,13 +57,13 @@ public class User {
                 .build();
     }
 
-    public User setNameAndPhoneNumberAndLocationAndEmail(String name, String phoneNumber, String email, String location) {
+    public User setNameAndPhoneNumberAndLocationAndEmail(String email, String name, String phoneNumber, String location) {
         return User.builder()
                 .id(this.id)
                 .email(email)
                 .password(this.password)
                 .name(name)
-                .profileImagePath(profileImagePath)
+                .profileImagePath(this.profileImagePath)
                 .phoneNumber(phoneNumber)
                 .location(location)
                 .isVisited(this.isVisited)
