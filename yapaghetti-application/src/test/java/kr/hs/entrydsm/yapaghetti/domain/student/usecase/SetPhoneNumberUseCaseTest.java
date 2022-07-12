0aece0bf958ea590d3a4965b08c1f65505136cf0
이준서 <ljchan71@gmail.com>
2022-07-12 23:01:56 +0900

    📑 :: SetPhoneNumberUseCaseTest

---
+++ yapaghetti-application/src/test/java/kr/hs/entrydsm/yapaghetti/domain/student/usecase/SetPhoneNumberUseCaseTest.java
@@ -0,0 +1,74 @@
+package kr.hs.entrydsm.yapaghetti.domain.student.usecase;
+
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.BDDMockito.given;
+
+import java.util.UUID;
+import kr.hs.entrydsm.yapaghetti.domain.auth.domain.AuthCode;
+import kr.hs.entrydsm.yapaghetti.domain.auth.exception.AuthCodeNotVerifiedException;
+import kr.hs.entrydsm.yapaghetti.domain.student.spi.StudentCommandUserPort;
+import kr.hs.entrydsm.yapaghetti.domain.student.spi.StudentQueryAuthCodePort;
+import kr.hs.entrydsm.yapaghetti.domain.student.spi.StudentQueryUserPort;
+import kr.hs.entrydsm.yapaghetti.domain.student.spi.StudentSecurityPort;
+import kr.hs.entrydsm.yapaghetti.domain.user.domain.User;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+@ExtendWith(MockitoExtension.class)
+public class SetPhoneNumberUseCaseTest {
+
+	@Mock
+	StudentQueryAuthCodePort studentQueryAuthCodePort;
+
+	@Mock
+	StudentQueryUserPort studentQueryUserPort;
+
+	@Mock
+	StudentSecurityPort studentSecurityPort;
+
+	@Mock
+	StudentCommandUserPort studentCommandUserPort;
+
+	@InjectMocks
+	SetPhoneNumberUseCase setPhoneNumberUseCase;
+
+	@Test
+	void 번호_설정() {
+		String phoneNumber = "01012345678";
+		UUID uuid = UUID.randomUUID();
+
+		given(studentQueryAuthCodePort.queryAuthCodeById(phoneNumber))
+			.willReturn(
+				AuthCode.builder()
+					.isVerify(true)
+					.build()
+			);
+		given(studentSecurityPort.getCurrentUserId())
+			.willReturn(uuid);
+		given(studentQueryUserPort.queryUserById(uuid))
+			.willReturn(
+				User.builder()
+					.phoneNumber(phoneNumber)
+					.build()
+			);
+
+		setPhoneNumberUseCase.execute(phoneNumber);
+	}
+
+	@Test
+	void 인증_미완료() {
+		String phoneNumber = "01012345678";
+
+		given(studentQueryAuthCodePort.queryAuthCodeById(phoneNumber))
+			.willReturn(
+				AuthCode.builder()
+					.isVerify(false)
+					.build()
+			);
+
+		assertThrows(AuthCodeNotVerifiedException.class, () -> setPhoneNumberUseCase.execute(phoneNumber));
+	}
+}
