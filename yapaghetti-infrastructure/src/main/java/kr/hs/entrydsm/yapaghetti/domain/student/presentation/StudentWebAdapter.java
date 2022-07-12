6256074e0a9c255e791627d24822348e756c6b3b
이준서 <ljchan71@gmail.com>
2022-07-12 23:02:38 +0900

    ⚡️ :: StudentWebAdapter

--- yapaghetti-infrastructure/src/main/java/kr/hs/entrydsm/yapaghetti/domain/student/presentation/StudentWebAdapter.java
+++ yapaghetti-infrastructure/src/main/java/kr/hs/entrydsm/yapaghetti/domain/student/presentation/StudentWebAdapter.java
@@ -1,5 +1,6 @@
 package kr.hs.entrydsm.yapaghetti.domain.student.presentation;
 
+import javax.validation.constraints.NotBlank;
 import kr.hs.entrydsm.yapaghetti.domain.student.api.QueryMyInfoPort;
 import kr.hs.entrydsm.yapaghetti.domain.student.api.ReflectFeedbackPort;
 import kr.hs.entrydsm.yapaghetti.domain.student.api.dto.response.MyInfoResponse;
@@ -10,6 +11,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -30,5 +32,11 @@ public class StudentWebAdapter {
 	@PatchMapping("/feedback/{sequence}")
 	public void reflectFeedback(@PathVariable("sequence") @NonNull Integer sequence) {
 		reflectFeedbackPort.execute(sequence);
+	}
+
+	@ResponseStatus(HttpStatus.NO_CONTENT)
+	@PatchMapping("/phone-number")
+	public void setPhoneNumber(@ResponseBody @NotBlank String phoneNumber) {
+
 	}
 }
