c5a3a619e8f2ee69689724647135043adf04ad30
이준서 <ljchan71@gmail.com>
2022-07-12 23:21:25 +0900

    ♻️ :: api 통합

--- yapaghetti-infrastructure/src/main/java/kr/hs/entrydsm/yapaghetti/global/security/SecurityConfig.java
+++ yapaghetti-infrastructure/src/main/java/kr/hs/entrydsm/yapaghetti/global/security/SecurityConfig.java
@@ -89,7 +89,7 @@ public class SecurityConfig {
                 // students
                 .antMatchers(HttpMethod.GET, "/students").hasRole(STUDENT)
                 .antMatchers(HttpMethod.PATCH, "/students/feedback/{sequence}").hasRole(STUDENT)
-                .antMatchers(HttpMethod.PATCH, "/students/phone-number").hasRole(STUDENT)
+                .antMatchers(HttpMethod.PATCH, "/students").hasRole(STUDENT)
 
                 // companies
                 .antMatchers(HttpMethod.PATCH, "/companies/name").hasAnyRole(MOU)
