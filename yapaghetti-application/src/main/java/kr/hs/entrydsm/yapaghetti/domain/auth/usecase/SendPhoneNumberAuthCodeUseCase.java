77468e322538f674c564b77e69270058e9b7b0a2
이준서 <ljchan71@gmail.com>
2022-07-12 11:30:49 +0900

    📑 :: SendPhoneNumberAuthCodeUseCase

---
+++ yapaghetti-application/src/main/java/kr/hs/entrydsm/yapaghetti/domain/auth/usecase/SendPhoneNumberAuthCodeUseCase.java
@@ -0,0 +1,59 @@
+package kr.hs.entrydsm.yapaghetti.domain.auth.usecase;
+
+import java.time.LocalDateTime;
+import kr.hs.entrydsm.yapaghetti.annotation.UseCase;
+import kr.hs.entrydsm.yapaghetti.domain.auth.api.SendPhoneNumberAuthCodePort;
+import kr.hs.entrydsm.yapaghetti.domain.auth.domain.AuthCode;
+import kr.hs.entrydsm.yapaghetti.domain.auth.exception.AuthCodeAlreadyTimeOutException;
+import kr.hs.entrydsm.yapaghetti.domain.auth.exception.AuthCodeAlreadyVerifiedException;
+import kr.hs.entrydsm.yapaghetti.domain.auth.spi.CommandAuthCodePort;
+import kr.hs.entrydsm.yapaghetti.domain.auth.spi.GenerateRandomStringPort;
+import kr.hs.entrydsm.yapaghetti.domain.auth.spi.GetAuthPropertiesPort;
+import kr.hs.entrydsm.yapaghetti.domain.auth.spi.QueryAuthCodePort;
+import kr.hs.entrydsm.yapaghetti.domain.auth.spi.SendSmsPort;
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
+@UseCase
+public class SendPhoneNumberAuthCodeUseCase implements SendPhoneNumberAuthCodePort {
+
+	private final QueryAuthCodePort queryAuthCodePort;
+	private final CommandAuthCodePort commandAuthCodePort;
+	private final GetAuthPropertiesPort getAuthPropertiesPort;
+	private final GenerateRandomStringPort generateRandomStringPort;
+	private final SendSmsPort sendSmsPort;
+
+	@Override
+	public void execute(String phoneNumber) {
+
+		String authCode = generateRandomStringPort.getAuthCode();
+		Long authTime = getAuthPropertiesPort.getAuthTime();
+		AuthCode authCodeDomain;
+
+		if (commandAuthCodePort.existsAuthCodeById(phoneNumber)) {
+			authCodeDomain = queryAuthCodePort.queryAuthCodeById(phoneNumber);
+
+			if (authCodeDomain.isVerify()) {
+				throw AuthCodeAlreadyVerifiedException.EXCEPTION;
+			}
+
+			if (authCodeDomain.getTimeToLive() > getAuthPropertiesPort.getLimitTime()) {
+				throw AuthCodeAlreadyTimeOutException.EXCEPTION;
+			}
+
+			authCodeDomain = authCodeDomain.refresh(authCode, authTime);
+		} else {
+			authCodeDomain = AuthCode.builder()
+				.value(phoneNumber)
+				.authCode(authCode)
+				.isVerify(false)
+				.timeToLive(authTime)
+				.authTime(LocalDateTime.now().plusSeconds(authTime))
+				.build();
+		}
+
+		commandAuthCodePort.saveAuthCode(authCodeDomain);
+
+		sendSmsPort.sendAuthCode(phoneNumber, authCode);
+	}
+}
