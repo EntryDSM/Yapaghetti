74ffda1d1ea3e7ad049f0d19e045c198fd6c37b5
osangu <hhgnoon@naver.com>
2022-07-14 22:25:53 +0900

    ♻️ ::  Getter

--- yapaghetti-application/src/main/java/kr/hs/entrydsm/yapaghetti/domain/teacher/api/dto/response/NewCompanyResponse.java
+++ yapaghetti-application/src/main/java/kr/hs/entrydsm/yapaghetti/domain/teacher/api/dto/response/NewCompanyResponse.java
@@ -1,7 +1,9 @@
 package kr.hs.entrydsm.yapaghetti.domain.teacher.api.dto.response;
 
+import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 
+@Getter
 @RequiredArgsConstructor
 public class NewCompanyResponse {
     private final String password;
