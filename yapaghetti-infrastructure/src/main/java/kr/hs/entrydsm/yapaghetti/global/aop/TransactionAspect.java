09549e76970218b896cf5beb0e0603dae2249b9d
KimBeomJin <sdpthf@naver.com>
2022-08-17 17:20:52 +0900

    ⚡️ :: TransactionManager로 변경

--- yapaghetti-infrastructure/src/main/java/kr/hs/entrydsm/yapaghetti/global/aop/TransactionAspect.java
+++ yapaghetti-infrastructure/src/main/java/kr/hs/entrydsm/yapaghetti/global/aop/TransactionAspect.java
@@ -1,19 +1,37 @@
 package kr.hs.entrydsm.yapaghetti.global.aop;
 
+import lombok.RequiredArgsConstructor;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Pointcut;
 import org.springframework.stereotype.Component;
-import org.springframework.transaction.annotation.Transactional;
+import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.TransactionStatus;
+import org.springframework.transaction.support.DefaultTransactionDefinition;
 
 @Aspect
 @Component
+@RequiredArgsConstructor
 public class TransactionAspect {
 
-    @Pointcut("annotation(kr.hs.entrydsm.yapaghetti.annotation.UseCase)")
-    public void applyTransaction() {
-        transaction();
+    private final PlatformTransactionManager transactionManager;
+
+    @Pointcut("execution(public * kr.hs.entrydsm.yapaghetti.annotation..*UseCase.*(..))")
+    public void getUseCases() {
     }
 
-    @Transactional
-    protected void transaction() { }
+    @Around("getUseCases()")
+    public Object applyTransaction(ProceedingJoinPoint joinPoint) {
+        TransactionStatus transaction = transactionManager.getTransaction(new DefaultTransactionDefinition());
+        try {
+            Object object = joinPoint.proceed();
+            transactionManager.commit(transaction);
+            return object;
+        } catch (Throwable e) {
+            e.printStackTrace();
+            transactionManager.rollback(transaction);
+            return null;
+        }
+    }
 }
