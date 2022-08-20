5af67926d35366afc4f3901dcf3ffb7d2d839878
osangu <hhgnoon@naver.com>
2022-08-20 17:55:32 +0900

    ♻️ :: Type 변경

--- yapaghetti-application/src/main/java/kr/hs/entrydsm/yapaghetti/domain/teacher/api/dto/response/CompanyElement.java
+++ yapaghetti-application/src/main/java/kr/hs/entrydsm/yapaghetti/domain/teacher/api/dto/response/CompanyElement.java
@@ -3,10 +3,12 @@ package kr.hs.entrydsm.yapaghetti.domain.teacher.api.dto.response;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
+import java.util.UUID;
+
 @Getter
 @AllArgsConstructor
 public class CompanyElement {
-    private final String companyId;
+    private final UUID companyId;
     private final String profileImagePath;
     private final String companyName;
     private final String email;
