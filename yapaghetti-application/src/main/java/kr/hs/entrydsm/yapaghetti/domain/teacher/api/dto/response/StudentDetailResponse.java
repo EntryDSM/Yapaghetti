b46d8d8ccaf4813b13da371317722ad53b49ff0c
osangu <hhgnoon@naver.com>
2022-08-31 17:57:31 +0900

    ♻️ :: profileImagePath 추가 & 생성자 변경

--- yapaghetti-application/src/main/java/kr/hs/entrydsm/yapaghetti/domain/teacher/api/dto/response/StudentDetailResponse.java
+++ yapaghetti-application/src/main/java/kr/hs/entrydsm/yapaghetti/domain/teacher/api/dto/response/StudentDetailResponse.java
@@ -9,14 +9,16 @@ public class StudentDetailResponse {
     private final String name;
     private final String gcn;
     private final String email;
+    private final String profileImagePath;
     private final String phoneNumber;
     private final String majorTagName;
     private final List<String> mySkillNameList;
 
-    public StudentDetailResponse(String name, String grade, String classNum, Integer number, String email,
-                                 String phoneNumber, String majorTagName, List<String> mySkillNameList) {
+    public StudentDetailResponse(String name, String grade, String classNum, Integer number, String profileImagePath,
+                                 String email, String phoneNumber, String majorTagName, List<String> mySkillNameList) {
         this.name = name;
         this.gcn = grade + classNum + String.format("%02d", number);
+        this.profileImagePath = profileImagePath;
         this.email = email;
         this.phoneNumber = phoneNumber;
         this.majorTagName = majorTagName;
