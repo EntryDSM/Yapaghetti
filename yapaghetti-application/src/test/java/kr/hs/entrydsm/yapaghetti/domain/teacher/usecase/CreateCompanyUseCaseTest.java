63ceee4a9dcb888e2cbbe88cb3a36a00e8db767f
정대현 <eogus_2513@dsm.hs.kr>
2022-09-03 11:20:04 +0900

    :test_tube: :: Mou & NonMou 추가

--- yapaghetti-application/src/test/java/kr/hs/entrydsm/yapaghetti/domain/teacher/usecase/CreateCompanyUseCaseTest.java
+++ yapaghetti-application/src/test/java/kr/hs/entrydsm/yapaghetti/domain/teacher/usecase/CreateCompanyUseCaseTest.java
@@ -16,7 +16,7 @@ import java.time.LocalDateTime;
 import static org.mockito.BDDMockito.given;
 
 @ExtendWith(MockitoExtension.class)
-public class CreateCompanyUseCaseTest {
+class CreateCompanyUseCaseTest {
 
     @Mock
     TeacherCommandUserPort teacherCommandUserPort;
@@ -37,7 +37,7 @@ public class CreateCompanyUseCaseTest {
     CreateCompanyUseCase createCompanyUseCase;
 
     @Test
-    public void execute() {
+    void 기업_생성_Mou() {
         LocalDateTime startAt = LocalDateTime.now();
         LocalDateTime endAt = LocalDateTime.now();
         String name = "testName";
@@ -48,24 +48,51 @@ public class CreateCompanyUseCaseTest {
 
         given(request.getName())
                 .willReturn(name);
-
         given(request.getCompanyName())
                 .willReturn(companyName);
-
         given(request.getLocation())
                 .willReturn(location);
-
         given(request.getEmail())
                 .willReturn(email);
-
         given(request.getPhoneNumber())
                 .willReturn(phoneNumber);
-
         given(request.getEndAt())
                 .willReturn(endAt);
-
         given(request.getStartAt())
                 .willReturn(startAt);
+        given(request.getIsMou())
+                .willReturn(true);
+
+        createCompanyUseCase.execute(request);
+
+    }
+
+    @Test
+    void 기업_생성_Not_Mou() {
+        LocalDateTime startAt = LocalDateTime.now();
+        LocalDateTime endAt = LocalDateTime.now();
+        String name = "testName";
+        String companyName = "entry";
+        String location = "대전";
+        String email = "entry@dsm.hs.kr";
+        String phoneNumber = "01011111111";
+
+        given(request.getName())
+                .willReturn(name);
+        given(request.getCompanyName())
+                .willReturn(companyName);
+        given(request.getLocation())
+                .willReturn(location);
+        given(request.getEmail())
+                .willReturn(email);
+        given(request.getPhoneNumber())
+                .willReturn(phoneNumber);
+        given(request.getEndAt())
+                .willReturn(endAt);
+        given(request.getStartAt())
+                .willReturn(startAt);
+        given(request.getIsMou())
+                .willReturn(false);
 
         createCompanyUseCase.execute(request);
 
