dc0fa49720d7a7edd36fe94c53c8c26d754f8dd1
정대현 <eogus_2513@dsm.hs.kr>
2022-09-03 11:11:56 +0900

    :bug: :: cascade 추가

--- yapaghetti-infrastructure/src/main/java/kr/hs/entrydsm/yapaghetti/domain/company/persistence/entity/CompanyEntity.java
+++ yapaghetti-infrastructure/src/main/java/kr/hs/entrydsm/yapaghetti/domain/company/persistence/entity/CompanyEntity.java
@@ -1,6 +1,5 @@
 package kr.hs.entrydsm.yapaghetti.domain.company.persistence.entity;
 
-import javax.persistence.OneToOne;
 import kr.hs.entrydsm.yapaghetti.domain.user.persistence.entity.UserEntity;
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
@@ -8,12 +7,14 @@ import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
+import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.FetchType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.MapsId;
+import javax.persistence.OneToOne;
 import javax.persistence.Table;
 import java.time.LocalDateTime;
 import java.util.UUID;
@@ -26,20 +27,20 @@ import java.util.UUID;
 @Entity
 public class CompanyEntity {
 
-	@Id
-	private UUID userId;
+    @Id
+    private UUID userId;
 
-	@MapsId
-	@OneToOne(fetch = FetchType.LAZY)
-	@JoinColumn(name = "user_id", columnDefinition = "BINARY(16)")
-	private UserEntity userEntity;
+    @MapsId
+    @OneToOne(cascade = CascadeType.ALL, fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id", columnDefinition = "BINARY(16)")
+    private UserEntity userEntity;
 
-	@Column(columnDefinition = "VARCHAR(40)", nullable = false)
-	private String companyName;
+    @Column(columnDefinition = "VARCHAR(40)", nullable = false)
+    private String companyName;
 
-	@Column(columnDefinition = "DATETIME", nullable = false)
-	private LocalDateTime startAt;
+    @Column(columnDefinition = "DATETIME", nullable = false)
+    private LocalDateTime startAt;
 
-	@Column(columnDefinition = "DATETIME", nullable = false)
-	private LocalDateTime endAt;
+    @Column(columnDefinition = "DATETIME", nullable = false)
+    private LocalDateTime endAt;
 }
