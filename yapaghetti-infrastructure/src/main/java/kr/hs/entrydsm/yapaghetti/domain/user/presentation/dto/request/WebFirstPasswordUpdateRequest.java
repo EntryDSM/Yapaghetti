c12de62308384ab42d0de0cc2b89d3cdabe0c5f5
osangu <hhgnoon@naver.com>
2022-09-08 13:18:39 +0900

    ♻️ :: 정규식 추가

--- yapaghetti-infrastructure/src/main/java/kr/hs/entrydsm/yapaghetti/domain/user/presentation/dto/request/WebFirstPasswordUpdateRequest.java
+++ yapaghetti-infrastructure/src/main/java/kr/hs/entrydsm/yapaghetti/domain/user/presentation/dto/request/WebFirstPasswordUpdateRequest.java
@@ -4,6 +4,7 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Pattern;
 import javax.validation.constraints.Size;
 
 @Getter
@@ -17,6 +18,8 @@ public class WebFirstPasswordUpdateRequest {
 
     @NotBlank
     @Size(max = 20)
+    @Pattern(regexp = "(?=.*[a-z])(?=.*[0-9])(?=.*[!#$%&'()*+,./:;<=>?@＼^_`{|}~])[a-zA-Z0-9!#$%&'()*+,./:;<=>?@＼^_`{|}~]{8,32}$",
+            message = "password는 소문자, 숫자, 특수문자가 포함되어야 합니다.")
     private String newPassword;
 
 }
