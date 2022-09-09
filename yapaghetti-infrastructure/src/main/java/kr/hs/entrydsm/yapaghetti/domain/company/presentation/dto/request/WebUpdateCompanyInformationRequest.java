51533017a340bc68489653357de17fc920a75f98
leeseojune53 <sung07288346@gmail.com>
2022-09-10 01:09:12 +0900

    ♻️ :: Request dto naming

--- yapaghetti-infrastructure/src/main/java/kr/hs/entrydsm/yapaghetti/domain/company/presentation/dto/request/WebUpdateCompanyInformationRequest.java
+++ yapaghetti-infrastructure/src/main/java/kr/hs/entrydsm/yapaghetti/domain/company/presentation/dto/request/WebUpdateCompanyInformationRequest.java
@@ -7,9 +7,21 @@ import javax.validation.constraints.NotBlank;
 
 @Getter
 @NoArgsConstructor
-public class WebUpdateCompanyNameRequest {
+public class WebUpdateCompanyInformationRequest {
 
     @NotBlank
     private String companyName;
+
+    @NotBlank
+    private String location;
+
+    @NotBlank
+    private String profileImagePath;
+
+    @NotBlank
+    private String name;
+
+    @NotBlank
+    private String phoneNumber;
 
 }
