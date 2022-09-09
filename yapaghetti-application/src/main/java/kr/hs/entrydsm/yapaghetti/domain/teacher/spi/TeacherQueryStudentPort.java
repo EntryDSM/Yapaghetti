9901a3dc7024a425b01907f2c142a02888f19513
osangu <hhgnoon@naver.com>
2022-09-10 01:15:20 +0900

    ♻️ :: queryPersonalAndMajorById

--- yapaghetti-application/src/main/java/kr/hs/entrydsm/yapaghetti/domain/teacher/spi/TeacherQueryStudentPort.java
+++ yapaghetti-application/src/main/java/kr/hs/entrydsm/yapaghetti/domain/teacher/spi/TeacherQueryStudentPort.java
@@ -4,6 +4,7 @@ import kr.hs.entrydsm.yapaghetti.domain.document.domain.DocumentType;
 import kr.hs.entrydsm.yapaghetti.domain.student.domain.Student;
 import kr.hs.entrydsm.yapaghetti.domain.teacher.api.dto.response.StudentDetailResponse;
 import kr.hs.entrydsm.yapaghetti.domain.teacher.api.dto.response.StudentElementByGradeClassNum;
+import kr.hs.entrydsm.yapaghetti.domain.teacher.api.dto.response.StudentPersonalAndMajorResponse;
 
 import java.util.List;
 import java.util.UUID;
@@ -11,7 +12,7 @@ import java.util.UUID;
 public interface TeacherQueryStudentPort {
     Student queryUserById(UUID studentId);
 
-    StudentDetailResponse queryStudentDetail(UUID studentId);
+    StudentPersonalAndMajorResponse queryPersonalAndMajorById(UUID studentId);
 
     List<StudentElementByGradeClassNum> queryStudentListByGradeAndClassNumAndDocStatus(
             Integer grade, Integer classNum, DocumentType docStatus
