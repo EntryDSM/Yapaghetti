8b7e3f1d27fd27f42760d040f8fa6ef5e20bce36
이준서 <ljchan71@gmail.com>
2022-09-11 16:47:21 +0900

    ⚡️ :: 커버리지 상승

--- yapaghetti-application/src/test/java/kr/hs/entrydsm/yapaghetti/domain/tag/usecase/SetMajorTagUseCaseTest.java
+++ yapaghetti-application/src/test/java/kr/hs/entrydsm/yapaghetti/domain/tag/usecase/SetMajorTagUseCaseTest.java
@@ -6,6 +6,7 @@ import kr.hs.entrydsm.yapaghetti.domain.student.spi.QueryStudentPort;
 import kr.hs.entrydsm.yapaghetti.domain.tag.api.dto.request.DomainSetMajorTagRequest;
 import kr.hs.entrydsm.yapaghetti.domain.tag.domain.Tag;
 import kr.hs.entrydsm.yapaghetti.domain.tag.domain.TagType;
+import kr.hs.entrydsm.yapaghetti.domain.tag.exception.InvalidTagTypeException;
 import kr.hs.entrydsm.yapaghetti.domain.tag.exception.TagNotFoundException;
 import kr.hs.entrydsm.yapaghetti.domain.tag.spi.QueryTagPort;
 import kr.hs.entrydsm.yapaghetti.domain.tag.spi.TagQueryStudentPort;
@@ -20,6 +21,7 @@ import org.mockito.junit.jupiter.MockitoExtension;
 import java.util.UUID;
 
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.BDDMockito.given;
 
 @ExtendWith(MockitoExtension.class)
@@ -62,6 +64,31 @@ public class SetMajorTagUseCaseTest {
         );
 
         setMajorTagUseCase.execute(new DomainSetMajorTagRequest(tagId));
+    }
+
+    @Test
+    void 다른_타입_태그() {
+        UUID userId = UUID.randomUUID();
+        UUID tagId = UUID.randomUUID();
+
+        given(tagSecurityPort.getCurrentUserId()).willReturn(userId);
+        given(tagQueryStudentPort.queryUserById(userId)).willReturn(
+            Student.builder()
+                .userId(userId)
+                .grade(3)
+                .classNum(1)
+                .number(13)
+                .tagId(UUID.randomUUID())
+                .build()
+        );
+        given(queryTagPort.queryTagById(tagId)).willReturn(
+            Tag.builder()
+                .type(TagType.SKILL)
+                .build()
+        );
+
+        assertThrows(InvalidTagTypeException.class,
+            () -> setMajorTagUseCase.execute(new DomainSetMajorTagRequest(tagId)));
     }
 
 }
