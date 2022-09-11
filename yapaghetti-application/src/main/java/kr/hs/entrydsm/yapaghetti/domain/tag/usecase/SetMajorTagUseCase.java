fdcc5bff8e3ec5ab6f59ae97377252f8fe55fc55
이준서 <ljchan71@gmail.com>
2022-09-11 16:25:33 +0900

    ♻️ :: 로직수정

--- yapaghetti-application/src/main/java/kr/hs/entrydsm/yapaghetti/domain/tag/usecase/SetMajorTagUseCase.java
+++ yapaghetti-application/src/main/java/kr/hs/entrydsm/yapaghetti/domain/tag/usecase/SetMajorTagUseCase.java
@@ -5,6 +5,9 @@ import kr.hs.entrydsm.yapaghetti.domain.student.domain.Student;
 import kr.hs.entrydsm.yapaghetti.domain.student.spi.CommandStudentPort;
 import kr.hs.entrydsm.yapaghetti.domain.tag.api.SetMajorTagPort;
 import kr.hs.entrydsm.yapaghetti.domain.tag.api.dto.request.DomainSetMajorTagRequest;
+import kr.hs.entrydsm.yapaghetti.domain.tag.domain.Tag;
+import kr.hs.entrydsm.yapaghetti.domain.tag.domain.TagType;
+import kr.hs.entrydsm.yapaghetti.domain.tag.exception.InvalidTagTypeException;
 import kr.hs.entrydsm.yapaghetti.domain.tag.spi.QueryTagPort;
 import kr.hs.entrydsm.yapaghetti.domain.tag.spi.TagQueryStudentPort;
 import kr.hs.entrydsm.yapaghetti.domain.tag.spi.TagSecurityPort;
@@ -26,7 +29,11 @@ public class SetMajorTagUseCase implements SetMajorTagPort {
     public void execute(DomainSetMajorTagRequest request) {
         Student student = tagQueryStudentPort.queryUserById(tagSecurityPort.getCurrentUserId());
 
-        queryTagPort.queryTagById(request.getTagId());
+        Tag tag = queryTagPort.queryTagById(request.getTagId());
+
+        if (tag.getType().equals(TagType.SKILL)) {
+            throw InvalidTagTypeException.EXCEPTION;
+        }
 
         commandStudentPort.saveStudent(student.updateTagId(request.getTagId()));
     }
