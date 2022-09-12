d3c897946b8e210f53881ebd0b74b14a6e9d26e5
이준서 <ljchan71@gmail.com>
2022-09-12 23:15:45 +0900

    ⚡️ :: 정대현님이 형식지키래요

--- yapaghetti-application/src/main/java/kr/hs/entrydsm/yapaghetti/domain/my_skill/spi/MySkillPort.java
+++ yapaghetti-application/src/main/java/kr/hs/entrydsm/yapaghetti/domain/my_skill/spi/MySkillPort.java
@@ -4,6 +4,5 @@ import kr.hs.entrydsm.yapaghetti.domain.student.spi.StudentQueryMySkillPort;
 import kr.hs.entrydsm.yapaghetti.domain.tag.spi.TagCommandMySkillPort;
 import kr.hs.entrydsm.yapaghetti.domain.tag.spi.TagQueryMySkillPort;
 
-public interface MySkillPort extends StudentQueryMySkillPort, TagQueryMySkillPort,
-	TagCommandMySkillPort {
+public interface MySkillPort extends StudentQueryMySkillPort, TagQueryMySkillPort, TagCommandMySkillPort {
 }
