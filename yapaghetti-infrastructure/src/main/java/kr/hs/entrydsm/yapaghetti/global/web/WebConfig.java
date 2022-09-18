2289971ba4a28092d164f32f10865f6e0959a650
정대현 <eogus_2513@dsm.hs.kr>
2022-09-19 06:36:52 +0900

    🔀 :: CORS 설정

--- yapaghetti-infrastructure/src/main/java/kr/hs/entrydsm/yapaghetti/global/web/WebConfig.java
+++ yapaghetti-infrastructure/src/main/java/kr/hs/entrydsm/yapaghetti/global/web/WebConfig.java
@@ -1,5 +1,6 @@
 package kr.hs.entrydsm.yapaghetti.global.web;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -7,12 +8,24 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
 
+    @Value("${cors.main-origin}")
+    private String mainOrigin;
+
+    @Value("${cors.user-origin}")
+    private String userOrigin;
+
+    @Value("${cors.teacher-origin}")
+    private String teacherOrigin;
+
+    @Value("${cors.company-origin}")
+    private String companyOrigin;
+
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
                 .allowedMethods("*")
                 .allowedOrigins("http://localhost:3000", "http://localhost:3001",
-                        "http://localhost:3002");
-        //TODO Repo에서 사용될 domain 추가필요.
+                        "http://localhost:3002", "http://localhost:3003",
+                        mainOrigin, userOrigin, teacherOrigin, companyOrigin);
     }
 }
