8e77e9a35770fa1ee111e5a2ab891beaa5844ced
이준서 <ljchan71@gmail.com>
2022-09-19 01:02:03 +0900

    ⚡️ :: sendMailPort

--- yapaghetti-application/src/main/java/kr/hs/entrydsm/yapaghetti/domain/auth/spi/SendMailPort.java
+++ yapaghetti-application/src/main/java/kr/hs/entrydsm/yapaghetti/domain/auth/spi/SendMailPort.java
@@ -1,5 +1,8 @@
 package kr.hs.entrydsm.yapaghetti.domain.auth.spi;
 
+import java.util.List;
+import kr.hs.entrydsm.yapaghetti.domain.auth.enums.EmailType;
+
 public interface SendMailPort {
-    void sendAuthCode(String email, String authCode);
+    void sendAuthCode(String email, List<String> values, EmailType emailType);
 }
