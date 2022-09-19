55e8b954df52dab46f929f1ad6603ce971e8e4ee
이준서 <ljchan71@gmail.com>
2022-09-19 18:55:00 +0900

    ⚡️ :: StudentWebAdapter

--- yapaghetti-infrastructure/src/main/java/kr/hs/entrydsm/yapaghetti/domain/student/presentation/StudentWebAdapter.java
+++ yapaghetti-infrastructure/src/main/java/kr/hs/entrydsm/yapaghetti/domain/student/presentation/StudentWebAdapter.java
@@ -1,11 +1,14 @@
 package kr.hs.entrydsm.yapaghetti.domain.student.presentation;
 
 import javax.validation.Valid;
+import kr.hs.entrydsm.yapaghetti.domain.student.api.BetaCreateUserPort;
 import kr.hs.entrydsm.yapaghetti.domain.student.api.QueryMyInfoPort;
 import kr.hs.entrydsm.yapaghetti.domain.student.api.ReflectFeedbackPort;
 import kr.hs.entrydsm.yapaghetti.domain.student.api.UpdateMyInfoPort;
+import kr.hs.entrydsm.yapaghetti.domain.student.api.dto.response.BetaCreateUserResponse;
 import kr.hs.entrydsm.yapaghetti.domain.student.api.dto.response.MyInfoResponse;
 import kr.hs.entrydsm.yapaghetti.domain.student.domain.UpdateType;
+import kr.hs.entrydsm.yapaghetti.domain.student.presentation.dto.request.BetaCreateStudentRequest;
 import kr.hs.entrydsm.yapaghetti.domain.student.presentation.dto.request.WebUpdateMyInfoRequest;
 import lombok.NonNull;
 import lombok.RequiredArgsConstructor;
@@ -13,6 +16,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -27,6 +31,7 @@ public class StudentWebAdapter {
 	private final QueryMyInfoPort queryMyInfoPort;
 	private final ReflectFeedbackPort reflectFeedbackPort;
 	private final UpdateMyInfoPort updateMyInfoPort;
+	private final BetaCreateUserPort betaCreateUserPort;
 
 	@GetMapping
 	public MyInfoResponse queryMyInfo() {
@@ -43,5 +48,12 @@ public class StudentWebAdapter {
 	@PatchMapping()
 	public void updateMyInfo(@RequestBody @Valid WebUpdateMyInfoRequest request, @RequestParam @NonNull UpdateType type) {
 		updateMyInfoPort.execute(request.getValue(), type);
+	}
+
+	@ResponseStatus(HttpStatus.CREATED)
+	@PostMapping("/beta")
+	public BetaCreateUserResponse betaCreateStudent(@RequestBody @Valid BetaCreateStudentRequest request) {
+		return betaCreateUserPort.execute(request.getEmail(), request.getClassNum(), request.getNumber(),
+			request.getName(), request.getPhoneNumber());
 	}
 }
