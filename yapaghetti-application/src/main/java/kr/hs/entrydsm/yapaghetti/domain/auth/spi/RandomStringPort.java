de651bbeba765af79ce218573ffa4d2dce945b11
이준서 <ljchan71@gmail.com>
2022-09-19 18:55:38 +0900

    ⚡️ :: RamdomStringPort

--- yapaghetti-application/src/main/java/kr/hs/entrydsm/yapaghetti/domain/auth/spi/RandomStringPort.java
+++ yapaghetti-application/src/main/java/kr/hs/entrydsm/yapaghetti/domain/auth/spi/RandomStringPort.java
@@ -1,6 +1,8 @@
 package kr.hs.entrydsm.yapaghetti.domain.auth.spi;
 
+import kr.hs.entrydsm.yapaghetti.domain.student.spi.StudentRandomStringPort;
 import kr.hs.entrydsm.yapaghetti.domain.teacher.spi.TeacherRandomStringPort;
 
-public interface RandomStringPort extends GenerateRandomStringPort, TeacherRandomStringPort {
+public interface RandomStringPort extends GenerateRandomStringPort, TeacherRandomStringPort,
+	StudentRandomStringPort {
 }
