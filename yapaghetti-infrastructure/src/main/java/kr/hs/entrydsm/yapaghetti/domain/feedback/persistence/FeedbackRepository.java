457ffdbd09fe3dec5e12f46d1bc851055477757f
osangu <hhgnoon@naver.com>
2022-09-21 02:05:09 +0900

    ♻️ :: deleteAllByIdDocumentId

--- yapaghetti-infrastructure/src/main/java/kr/hs/entrydsm/yapaghetti/domain/feedback/persistence/FeedbackRepository.java
+++ yapaghetti-infrastructure/src/main/java/kr/hs/entrydsm/yapaghetti/domain/feedback/persistence/FeedbackRepository.java
@@ -1,5 +1,6 @@
 package kr.hs.entrydsm.yapaghetti.domain.feedback.persistence;
 
+import kr.hs.entrydsm.yapaghetti.domain.document.persistence.entity.DocumentEntity;
 import kr.hs.entrydsm.yapaghetti.domain.feedback.persistence.entity.FeedbackEntity;
 import kr.hs.entrydsm.yapaghetti.domain.feedback.persistence.entity.FeedbackEntityId;
 import org.springframework.data.repository.CrudRepository;
@@ -11,4 +12,6 @@ import java.util.UUID;
 public interface FeedbackRepository extends CrudRepository<FeedbackEntity, FeedbackEntityId> {
     Optional<FeedbackEntity> findByIdDocumentIdAndIdSequence(UUID id_documentId, Integer id_sequence);
 	List<FeedbackEntity> findByIdDocumentId(UUID id_documentId);
+
+    void deleteAllByIdDocumentId(UUID documentId);
 }
