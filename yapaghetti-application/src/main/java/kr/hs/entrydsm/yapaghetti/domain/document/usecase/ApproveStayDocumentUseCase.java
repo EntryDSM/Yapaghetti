990ac5978cb92c63cba30d54a9f6599cd6c27b45
osangu <hhgnoon@naver.com>
2022-09-21 02:08:18 +0900

    ♻️ :: 로직 수정

--- yapaghetti-application/src/main/java/kr/hs/entrydsm/yapaghetti/domain/document/usecase/ApproveStayDocumentUseCase.java
+++ yapaghetti-application/src/main/java/kr/hs/entrydsm/yapaghetti/domain/document/usecase/ApproveStayDocumentUseCase.java
@@ -4,7 +4,9 @@ import kr.hs.entrydsm.yapaghetti.annotation.UseCase;
 import kr.hs.entrydsm.yapaghetti.domain.document.api.ApproveStayDocumentPort;
 import kr.hs.entrydsm.yapaghetti.domain.document.domain.Document;
 import kr.hs.entrydsm.yapaghetti.domain.document.domain.DocumentType;
+import kr.hs.entrydsm.yapaghetti.domain.document.exception.DocumentNotFoundException;
 import kr.hs.entrydsm.yapaghetti.domain.document.spi.CommandDocumentPort;
+import kr.hs.entrydsm.yapaghetti.domain.document.spi.DocumentCommandFeedbackPort;
 import kr.hs.entrydsm.yapaghetti.domain.document.spi.QueryDocumentPort;
 import lombok.RequiredArgsConstructor;
 
@@ -16,13 +18,25 @@ public class ApproveStayDocumentUseCase implements ApproveStayDocumentPort {
 
     private final QueryDocumentPort queryDocumentPort;
     private final CommandDocumentPort commandDocumentPort;
+    private final DocumentCommandFeedbackPort documentCommandFeedbackPort;
 
     @Override
     public void execute(UUID studentId) {
         Document stayDocument = queryDocumentPort.queryDocumentByUserIdAndType(studentId, DocumentType.STAY);
 
+        if (queryDocumentPort.checkDocumentByUserIdAndType(studentId, DocumentType.PUBLIC)) {
+            commandDocumentPort.deleteDocument(
+                    queryDocumentPort.queryDocumentByUserIdAndType(studentId, DocumentType.PUBLIC)
+            );
+        }
+
+        documentCommandFeedbackPort.deleteAllByDocumentId(
+                stayDocument.getId()
+        );
+
         commandDocumentPort.saveDocument(
                 stayDocument.changeDocumentType(DocumentType.PUBLIC)
         );
+
     }
 }
