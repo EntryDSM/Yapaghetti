32c4ded538df3dc42e12b1f608b36516e45848cd
osangu <hhgnoon@naver.com>
2022-09-22 19:00:37 +0900

    ♻️ :: 함수 변경

--- yapaghetti-application/src/main/java/kr/hs/entrydsm/yapaghetti/domain/student/usecase/QueryMyInfoUseCase.java
+++ yapaghetti-application/src/main/java/kr/hs/entrydsm/yapaghetti/domain/student/usecase/QueryMyInfoUseCase.java
@@ -3,19 +3,18 @@ package kr.hs.entrydsm.yapaghetti.domain.student.usecase;
 import kr.hs.entrydsm.yapaghetti.annotation.ReadOnlyUseCase;
 import kr.hs.entrydsm.yapaghetti.domain.student.api.QueryMyInfoPort;
 import kr.hs.entrydsm.yapaghetti.domain.student.api.dto.response.MyInfoResponse;
+import kr.hs.entrydsm.yapaghetti.domain.student.api.dto.response.MySkillElement;
 import kr.hs.entrydsm.yapaghetti.domain.student.domain.Student;
 import kr.hs.entrydsm.yapaghetti.domain.student.spi.QueryStudentPort;
 import kr.hs.entrydsm.yapaghetti.domain.student.spi.StudentQueryMySkillPort;
 import kr.hs.entrydsm.yapaghetti.domain.student.spi.StudentQueryTagPort;
 import kr.hs.entrydsm.yapaghetti.domain.student.spi.StudentQueryUserPort;
 import kr.hs.entrydsm.yapaghetti.domain.student.spi.StudentSecurityPort;
-import kr.hs.entrydsm.yapaghetti.domain.tag.domain.Tag;
 import kr.hs.entrydsm.yapaghetti.domain.user.domain.User;
 import lombok.RequiredArgsConstructor;
 
 import java.util.List;
 import java.util.UUID;
-import java.util.stream.Collectors;
 
 @RequiredArgsConstructor
 @ReadOnlyUseCase
@@ -35,10 +34,7 @@ public class QueryMyInfoUseCase implements QueryMyInfoPort {
 
         User user = studentQueryUserPort.queryUserById(userId);
 
-        List<String> skillTagList = studentQueryMySkillPort.queryMySkillByUserId(userId).stream()
-                .map(mySkill -> studentQueryTagPort.queryTagById(mySkill.getTagId()))
-                .map(Tag::getName)
-                .collect(Collectors.toList());
+        List<MySkillElement> skillTagList = studentQueryMySkillPort.queryTagIdAndNameByUserId(userId);
 
         String majorTag = studentQueryTagPort.queryTagById(student.getTagId()).getName();
 
