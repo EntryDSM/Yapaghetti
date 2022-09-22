21daa83444db4d3bc1fc63844c63762e579cc09c
osangu <hhgnoon@naver.com>
2022-09-22 18:59:54 +0900

    ♻️ :: queryNameAndIdByUserId

--- yapaghetti-application/src/main/java/kr/hs/entrydsm/yapaghetti/domain/student/spi/StudentQueryMySkillPort.java
+++ yapaghetti-application/src/main/java/kr/hs/entrydsm/yapaghetti/domain/student/spi/StudentQueryMySkillPort.java
@@ -1,10 +1,14 @@
 package kr.hs.entrydsm.yapaghetti.domain.student.spi;
 
 import kr.hs.entrydsm.yapaghetti.domain.my_skill.domain.MySkill;
+import kr.hs.entrydsm.yapaghetti.domain.student.api.dto.response.MySkillElement;
 
 import java.util.List;
 import java.util.UUID;
 
 public interface StudentQueryMySkillPort {
     List<MySkill> queryMySkillByUserId(UUID userId);
+
+    List<MySkillElement> queryTagIdAndNameByUserId(UUID userId);
+
 }
