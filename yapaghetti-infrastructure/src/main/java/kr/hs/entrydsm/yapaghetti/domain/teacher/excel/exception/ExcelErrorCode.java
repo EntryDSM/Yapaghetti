5a69dfee76c11b04a792603811565c9b09d71b9c
osangu <hhgnoon@naver.com>
2022-10-19 12:24:47 +0900

    ♻️ :: status 변경

--- yapaghetti-infrastructure/src/main/java/kr/hs/entrydsm/yapaghetti/domain/teacher/excel/exception/ExcelErrorCode.java
+++ yapaghetti-infrastructure/src/main/java/kr/hs/entrydsm/yapaghetti/domain/teacher/excel/exception/ExcelErrorCode.java
@@ -9,7 +9,7 @@ import lombok.Getter;
 @AllArgsConstructor(access = AccessLevel.PRIVATE)
 public enum ExcelErrorCode implements ErrorProperty {
 
-    EXCEL_IO_EXCEPTION(400, "엑셀을 다운로드 할 수 없습니다.");
+    EXCEL_IO_EXCEPTION(500, "엑셀을 다운로드 할 수 없습니다.");
 
     private final int status;
     private final String message;
