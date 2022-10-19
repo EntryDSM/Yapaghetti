2788bd93b65da0f6921de05bc1a96ff0a7cfd7a1
osangu <hhgnoon@naver.com>
2022-10-19 11:03:56 +0900

    📑 :: CreateExcelInterface

---
+++ yapaghetti-infrastructure/src/main/java/kr/hs/entrydsm/yapaghetti/domain/teacher/excel/CreateExcelInterface.java
@@ -0,0 +1,7 @@
+package kr.hs.entrydsm.yapaghetti.domain.teacher.excel;
+
+import javax.servlet.http.HttpServletResponse;
+
+public interface CreateExcelInterface {
+    void execute(HttpServletResponse response);
+}
