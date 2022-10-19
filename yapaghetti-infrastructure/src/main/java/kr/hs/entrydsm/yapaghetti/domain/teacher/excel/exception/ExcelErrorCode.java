6925369643cc2555b5af9c6e9b6753ed587a6cf6
osangu <hhgnoon@naver.com>
2022-10-19 11:03:27 +0900

    📑 :: ExcelErrorcode

---
+++ yapaghetti-infrastructure/src/main/java/kr/hs/entrydsm/yapaghetti/domain/teacher/excel/exception/ExcelErrorCode.java
@@ -0,0 +1,16 @@
+package kr.hs.entrydsm.yapaghetti.domain.teacher.excel.exception;
+
+import kr.hs.entrydsm.yapaghetti.error.ErrorProperty;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+public enum ExcelErrorCode implements ErrorProperty {
+
+    EXCEL_IO_EXCEPTION(400, "엑셀을 다운로드 할 수 없습니다.");
+
+    private final int status;
+    private final String message;
+}
